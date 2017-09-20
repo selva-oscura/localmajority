be2fbc330a108f0ff3d71d49e534e108921fa33f
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-20 15:37:13 -0700

    added css for Aside

--- src/components/common/Aside.js
+++ src/components/common/Aside.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Aside.css';
 import TwitterTimeline from './TwitterTimeline';
+
 
 const Aside = (props) => {
 	console.log('props', props)
 	const twitterHandles = props.twitterHandles;
 	return (
-		<aside>
+		<aside id="Aside">
 			{
 				twitterHandles.map((twitterHandle, i) => (
 					<TwitterTimeline 
