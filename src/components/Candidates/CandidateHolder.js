5359228c18bcc6bf5709b9bdb75b5181fba99b6e
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-20 22:27:11 -0700

    replaced filter of candidates data with reliance on candidate object (passed by Router in index.js)

--- src/components/Candidates/CandidateHolder.js
+++ src/components/Candidates/CandidateHolder.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Candidate from './Candidate';
 import NoSuchCandidate from './NoSuchCandidate';
-import candidates from '../../data/candidates.json';
-// import './CandidateHolder.css';
-// console.log('candidates from CandidateHolder', candidates);
 
 const CandidateHolder = (props) => {
-	const validCandidate = (candidate) => (candidate.id === props.match.params.id);
-	const candidate = candidates.filter(validCandidate);
+	const candidate = props.candidate;
+	document.title = `Local Majority - ${candidate.title}`;
   return (
     <div className="CandidateHolder">
-    	{ candidate.length===0 && <NoSuchCandidate candidateId={props.match.params.id} />}
-    	{ candidate.length===1 && <Candidate candidateId={props.match.params.id} candidate={candidate[0]} />}
+    	{ candidate ? (
+    			<Candidate candidateId={props.match.params.id} candidate={candidate} {...props} />
+    		) : (
+    			<NoSuchCandidate candidateId={props.match.params.id} />
+    		)}
     </div>
   );
 };
