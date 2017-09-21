a205024140ae8040cdea9a0d084f0758c62af69e
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-20 22:31:01 -0700

    updated DistrictHolder's switching to reflect filtering now done by Router

--- src/components/Districts/DistrictHolder.js
+++ src/components/Districts/DistrictHolder.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import District from './District';
 import NoSuchDistrict from './NoSuchDistrict';
-import districts from '../../data/districts.json';
-// import './DistrictHolder.css';
 
 const DistrictHolder = (props) => {
-	// const districtCheck = districts.map((district) => district.title.slice("District ".length));
-	const validDistrict = (district) => (district.title.slice("District ".length) === props.match.params.id);
-	const districtData = districts.filter(validDistrict);
+	const district = props.district;
   return (
     <div className="DistrictHolder">
-    	{ districtData.length===0 && <NoSuchDistrict districtId={props.match.params.id} />}
-    	{ districtData.length===1 && <District districtId={props.match.params.id} district={districtData[0]} />}
+			{ district ? (
+					<District districtId={props.match.params.id} district={district} {...props} />
+				) : (
+					<NoSuchDistrict districtId={props.match.params.id} />
+				)
+			}
     </div>
   );
 };
