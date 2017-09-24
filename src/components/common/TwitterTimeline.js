7a0510ae8b4debdade00fd8f7deebda7993ab8e3
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-23 23:40:30 -0700

    added div, positioning, css to mask 'embed' link from twitter's boilerplate Timeline

--- src/components/common/TwitterTimeline.js
+++ src/components/common/TwitterTimeline.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Timeline } from 'react-twitter-widgets'
+import './TwitterTimeline.css';
 
 const TwitterTimeline = (props) => {
 	const twitterHandle = props.twitterHandle;
 	return (
-		<Timeline
-		  dataSource={{
-		    sourceType: 'profile',
-		    screenName: twitterHandle
-		  }}
-		  options={{
-		    height: '500',
-		  }}
-		/>
-	)
-}
+		<div className="TwitterTimeline">
+			<Timeline
+			  dataSource={{
+			    sourceType: 'profile',
+			    screenName: twitterHandle
+			  }}
+			  options={{
+			    height: '500',
+			  }}
+			/>
+			<div className="mask"></div>
+		</div>
+	);
+};
 
 export default TwitterTimeline;
