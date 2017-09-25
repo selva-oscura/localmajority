479328d30c792ac3477d967eae84a427e58988b9
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-24 18:43:19 -0700

    update document title on route change

--- src/components/Issues/Issues.js
+++ src/components/Issues/Issues.js
@@ -4,6 +4,7 @@ import Card from '../common/Card';
 const Issues = (props) => {
   console.log('props for Issues', props);
   const issues = props.issues;
+  document.title = "Local Majority | Issues";
 
   return (
     <div className="Issues flex">
