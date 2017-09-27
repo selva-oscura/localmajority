ee57895448de6707ac19ce207f3484a88ca4a3ff
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-26 20:11:52 -0700

    updated info re: button; added error message if neither link string and nor handleClick function passed

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import './Button.css';
 
 const Button = ({link, kind='primary', label, handleClick}) => {
+	// either link or handleClick is required
 	// link -- string -- include http or https for offsite or relative link (omitting domain name) for onsite
 	// kind -- string --
 	//				 options -- primary, secondary, danger
 	// label -- string -- text inside button
+	// handleClick -- function to be executed onClick
 	let button;
+	if (!link && !handleClick) {
+		console.log('need either a link or a handleClick to make Button');
+		return null;
+	}
 	if (link) {
 		button = link.slice(0,4)==="http" ? (
 	    <a href={`${link}`}>
