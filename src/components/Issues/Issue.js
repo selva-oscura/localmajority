40daa8ea17f17d6acf92fae877d606abf5bf06b5
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-26 20:10:05 -0700

    added fetch & display  article's related html file from server

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import axios from 'axios';
+import issues from '../../data/issues.json';
 
-const Issue = (props) => {
-	console.log('props from Issue', props);
-  const issue = props.issue;
-  return (
-    <div className="Issue">
-    	<h2>{issue.title}</h2>
-    	<h3>by {issue.author}</h3>
-      <h6>{issue.time.slice(0, issue.time.length - 15)} -- {Math.floor(issue.contentLength / 400)} minute read</h6>
-      <p>Summary: {issue.summary}</p>
-    </div>
-  );
-};
-
-Issue.propTypes = {
-  issue: PropTypes.object.isRequired,
-};
+class Issue extends Component {
+  componentDidMount(){
+    const url = "http://selva-oscura.net/projects/localMajority/articles/";
+    const param = this.props.match.params.id;
+    axios.get(url+param+".html")
+    .then(res => {
+      if (res.status === 200) {
+        document.getElementById('article-space').innerHTML = res.data;
+      } else {
+        console.log('error fetching', param, 'status', res.status, '\n', res);
+        document.getElementById('article-space').innerHTML = "<p>We\'re sorry, but there was an error fetching your article.</p>";
+      }
+    }).catch(err => {
+      console.log('error fetching', param, err);
+      document.getElementById('article-space').innerHTML = "<p>We\'re sorry, but there was an error fetching your article.</p>";
+    });
+  }
+  render(){
+    const issue = this.props.issue;
+    // console.log('props from Issue', this.props);
+    // console.log('issue from Issue', issue);
+    return (
+      <div className="Issue">
+        <h2>{issue.title}</h2>
+        <h3>by {issue.author}</h3>
+        <h6>{issue.time.slice(0, issue.time.length - 15)} -- {Math.floor(issue.contentLength / 400)} minute read</h6>
+        <p><b>Summary:</b> {issue.summary}</p>
+        <div id="article-space"></div>
+      </div>
+    )
+  }
+}
 
 export default Issue;
