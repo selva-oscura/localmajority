ca185260d15c20677f95aef9de3fc0111595ff60
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-09-26 19:46:59 -0700

    changed className for main-and-aside; added stand-in text for relevant content;

--- src/components/Candidates/Candidate.js
+++ src/components/Candidates/Candidate.js
@@ -18,7 +18,7 @@ const Candidate = (props) => {
 		facebook: "JenniferCarrollFoy",
 	});
   return (
-    <div className="Candidate flex">
+    <div className="Candidate main-and-aside">
     	<article className="Main">
 	    	<h2>{candidate.title}</h2>
 	    	<h3>{candidate.district}</h3>
@@ -27,6 +27,29 @@ const Candidate = (props) => {
 	    		alt={candidate.title} 
 	    	/>
 	    	<p>{candidate.summary}</p>
+        <div className="row">
+          <h2>Why this Race Matters</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        </div>
+        <div className="row">
+          <h2>Know the District</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        </div>
+        <div className="row">
+          <h2>Comparing Candidates</h2>
+          <div className="row">
+            <div className="half">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            </div>
+            <div className="half">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <h2>Why &amp; Why Not</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        </div>
     	</article>
     	<CandidateAside 
     		candidate={candidate}
