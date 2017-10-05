0c81555566ebb95e57e3e591c4e570bf64c82ee4
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-04 19:33:23 -0700

    created cms queries (get all or one)

---
+++ src/api/cmsQueries.js
@@ -0,0 +1,9 @@
+const cmsPath = "https://cmsdev.localmajority.net/api";
+
+const cmsQueries = {
+	getAll: (field) => (`${cmsPath}/${field}`),
+	getOneById: (uid) => (`${cmsPath}/id/${uid}`),
+	getOneByPath: (urlPath) => (`${cmsPath}/${urlPath}`),
+};
+
+export default cmsQueries;
