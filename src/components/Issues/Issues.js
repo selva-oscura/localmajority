e3a15dbe33451fb586f58412ba3de8e7d43851a7
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:40:04 -0700

    linting (no changes to substance)

--- src/components/Issues/Issues.js
+++ src/components/Issues/Issues.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Card from '../common/Card';
 
-const Issues = (props) => {
+const Issues = props => {
   console.log('props for Issues', props);
   const issues = props.issues;
-  document.title = "Local Majority | Issues";
+  document.title = 'Local Majority | Issues';
 
   return (
     <div className="Issues flex">
-    	{issues.map((issue, i) => (
-    		<Card
-    			key={i}
-    			title={issue.title}
-    			subtitle={`by ${issue.author}`}
-    			text={issue.summary}
-    			imgSrc={issue.imgSrc}
-    			category="issues"
-    			id={issue.id}
-    			imgShape="landscape"
-    		/>
-    	))}
+      {issues.map((issue, i) => (
+        <Card
+          key={i}
+          title={issue.title}
+          subtitle={`by ${issue.author}`}
+          text={issue.summary}
+          imgSrc={issue.imgSrc}
+          category="issues"
+          id={issue.id}
+          imgShape="landscape"
+        />
+      ))}
     </div>
   );
 };
