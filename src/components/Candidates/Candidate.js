b25398aff0b22afe2bbcaaa797c1e06d0f1f65b4
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:45:05 -0700

    linting (no changes to substance)

--- src/components/Candidates/Candidate.js
+++ src/components/Candidates/Candidate.js
@@ -2,60 +2,111 @@ import React from 'react';
 import CandidateAside from './CandidateAside';
 import './Candidate.css';
 
-const Candidate = (props) => {
+const Candidate = props => {
   // return <p>candidate</p>;
-	console.log('props from Candidate',props);
-	let candidate = props.candidate;
-	// candidate = Object.assign({}, candidate, {
-	// 	address: {
-	// 		street: "PO Box 5113",
- // 			city: "Woodbridge",
-	// 		state: "VA",
-	// 		zip: "22194",
-	// 	},
-	// 	phone: "571-989-1713",
-	// 	fax: null,
-	// 	email: "jennifercarrollfoy@gmail.com",
-	// 	twitter: "JCarrollFoy",
-	// 	facebook: "JenniferCarrollFoy",
-	// });
+  console.log('props from Candidate', props);
+  let candidate = props.candidate;
+  // candidate = Object.assign({}, candidate, {
+  // 	address: {
+  // 		street: "PO Box 5113",
+  // 			city: "Woodbridge",
+  // 		state: "VA",
+  // 		zip: "22194",
+  // 	},
+  // 	phone: "571-989-1713",
+  // 	fax: null,
+  // 	email: "jennifercarrollfoy@gmail.com",
+  // 	twitter: "JCarrollFoy",
+  // 	facebook: "JenniferCarrollFoy",
+  // });
   return (
     <div className="Candidate">
       <article>
         <div className="row">
           <div className="brief-info">
             <h2>{candidate.title}</h2>
-            <h3>{candidate.district ? candidate.district : "no district data"}</h3>
-            <p className="hide-on-small">{candidate.descriptionText ? candidate.descriptionText : "no descriptionText"}</p>
+            <h3>
+              {candidate.district ? candidate.district : 'no district data'}
+            </h3>
+            <p className="hide-on-small">
+              {candidate.descriptionText
+                ? candidate.descriptionText
+                : 'no descriptionText'}
+            </p>
           </div>
           <div className="headshot">
-            <img
-              src={`../${candidate.headshotSm.url}`}
-              alt={candidate.title}
-            />
+            <img src={`../${candidate.headshotSm.url}`} alt={candidate.title} />
           </div>
-          <p className="show-on-small">{candidate.descriptionText ? candidate.descriptionText : "no descriptionText"}</p>
+          <p className="show-on-small">
+            {candidate.descriptionText
+              ? candidate.descriptionText
+              : 'no descriptionText'}
+          </p>
         </div>
         <div className="main-and-aside">
           <div className="Main">
             <div className="row">
               <h2>Why this Race Matters</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum
+              </p>
             </div>
             <div className="row">
               <h2>Comparing Candidates</h2>
               <div className="row">
                 <div className="half">
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum
+                  </p>
                 </div>
                 <div className="half">
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum
+                  </p>
                 </div>
               </div>
             </div>
             <div className="row">
               <h2>Why &amp; Why Not</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum
+              </p>
             </div>
           </div>
           <CandidateAside candidate={candidate} />
--- src/components/Candidates/CandidateAside.js
+++ src/components/Candidates/CandidateAside.js
@@ -3,49 +3,27 @@ import '../common/Aside.css';
 import TwitterTimeline from '../common/TwitterTimeline';
 import { SocialIcon } from 'react-social-icons';
 
-const CandidateAside = (props) => {
-	console.log('props from Aside', props);
-	const candidate=props.candidate;
-	const twitterHandles = props.twitterHandles;
-	return (
-		<aside id="Aside">
-			<TwitterTimeline 
-				twitterHandle={[candidate.twitter]}
-			/>
-			<h3>Contact the Candidate</h3>
-			<div className="row">
-				{
-					candidate.twitter ? (
-						<SocialIcon 
-							url={`https://twitter.com/${candidate.twitter}`}
-						/>
-					) : (
-						null
-					)						
-				}
-				{
-					candidate.facebook ? (
-						<SocialIcon 
-							url={`https://www.facebook.com/${candidate.facebook}`}
-						/>
-					) : (
-						null
-					)
-				}
-				{
-					candidate.email ? (
-						<SocialIcon
-							url={`mailto:${candidate.email}`}
-							network="email"
-						/>
-					) : (
-						null
-					)
-				}
-
-			</div>
-			</aside>
-	)
-}
+const CandidateAside = props => {
+  console.log('props from Aside', props);
+  const candidate = props.candidate;
+  const twitterHandles = props.twitterHandles;
+  return (
+    <aside id="Aside">
+      <TwitterTimeline twitterHandle={[candidate.twitter]} />
+      <h3>Contact the Candidate</h3>
+      <div className="row">
+        {candidate.twitter ? (
+          <SocialIcon url={`https://twitter.com/${candidate.twitter}`} />
+        ) : null}
+        {candidate.facebook ? (
+          <SocialIcon url={`https://www.facebook.com/${candidate.facebook}`} />
+        ) : null}
+        {candidate.email ? (
+          <SocialIcon url={`mailto:${candidate.email}`} network="email" />
+        ) : null}
+      </div>
+    </aside>
+  );
+};
 
 export default CandidateAside;
