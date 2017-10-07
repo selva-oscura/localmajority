e7aab0ed668f079e4f55a0cf4a4433f1fe1c8a84
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:42:35 -0700

    linting (no changes to substance)

--- src/components/Districts/Districts.js
+++ src/components/Districts/Districts.js
@@ -2,11 +2,11 @@ import React from 'react';
 // import districts from '../../data/districts.json';
 import Card from '../common/Card';
 
-const Districts = (props) => {
+const Districts = props => {
   const districts = props.districts;
-	console.log("props for Districts", props);
-  document.title = "Local Majority | Districts";
- //  console.log('districts', districts);
+  console.log('props for Districts', props);
+  document.title = 'Local Majority | Districts';
+  //  console.log('districts', districts);
   return (
     <div className="Districts flex">
       {districts.map((district, i) => (
@@ -16,7 +16,7 @@ const Districts = (props) => {
           text={district.summary}
           imgSrc={district.imgSrc}
           category="districts"
-          id={district.title.slice("District ".length)}
+          id={district.title.slice('District '.length)}
           imgShape="landscape"
         />
       ))}
