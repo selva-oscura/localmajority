f733fa5cfe0fc065991889c46587eb513089906e
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:26:53 -0700

    linting (no changes to substance)

--- src/api/cmsQueries.js
+++ src/api/cmsQueries.js
@@ -1,9 +1,9 @@
-const cmsPath = "https://cmsdev.localmajority.net/api";
+const cmsPath = 'https://cmsdev.localmajority.net/api';
 
 const cmsQueries = {
-	getAll: (field) => (`${cmsPath}/${field}`),
-	getOneById: (uid) => (`${cmsPath}/id/${uid}`),
-	getOneByPath: (urlPath) => (`${cmsPath}/${urlPath}`),
+  getAll: field => `${cmsPath}/${field}`,
+  getOneById: uid => `${cmsPath}/id/${uid}`,
+  getOneByPath: urlPath => `${cmsPath}/${urlPath}`,
 };
 
 export default cmsQueries;
--- src/api/queryFields.js
+++ src/api/queryFields.js
@@ -1,14 +1,49 @@
 const queryFields = [
-  {tableName:"Article", stateName:"articles", all: "articles", one: "article"},
-  {tableName:"Candidate", stateName:"candidates", all:"candidates", one: "candidate"},
-  {tableName:"Contest", stateName:"contests", all:"contests", one: "contest"},
+  {
+    tableName: 'Article',
+    stateName: 'articles',
+    all: 'articles',
+    one: 'article',
+  },
+  {
+    tableName: 'Candidate',
+    stateName: 'candidates',
+    all: 'candidates',
+    one: 'candidate',
+  },
+  {
+    tableName: 'Contest',
+    stateName: 'contests',
+    all: 'contests',
+    one: 'contest',
+  },
   // {tableName:"District", stateName:"districts", all:"districts", one: "district"},
   // {tableName:"Issue", stateName:"issues", all:"issues", one: "issue"},
-  {tableName:"IssuePrimers", stateName:"issuePrimers", all:"issue-primers", one: "issue-primer"},
-  {tableName:"Party", stateName:"parties", all: "parties", one: "party"},
-  {tableName:"Seat", stateName:"seats", all: "seats", one: "seat"},
+  {
+    tableName: 'IssuePrimers',
+    stateName: 'issuePrimers',
+    all: 'issue-primers',
+    one: 'issue-primer',
+  },
+  {
+    tableName: 'Party',
+    stateName: 'parties',
+    all: 'parties',
+    one: 'party'
+  },
+  {
+    tableName: 'Seat',
+    stateName: 'seats',
+    all: 'seats',
+    one: 'seat'
+  },
   // {tableName:"State", stateName:"states", all: "states", one: "state"},
-  {tableName:"TalkingPoints", stateName:"talkingPoints", all: "talking-points", one: "talking-point"},
+  {
+    tableName: 'TalkingPoints',
+    stateName: 'talkingPoints',
+    all: 'talking-points',
+    one: 'talking-point',
+  },
 ];
 
 export default queryFields;
