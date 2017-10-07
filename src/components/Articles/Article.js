4bc56ea387f9fbf0b2ac4aa725d927ae9a356bb8
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:48:31 -0700

    appending of multiple html pages (articles) to page

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,15 +1,45 @@
+// import React from 'react';
 import React, { Component } from 'react';
 
 class Article extends Component {
-	render() {
-		console.log('props', this.props);
-		return (
-			<article className="Article">
-				article
-			</article>
-		)
-	}
-
+  // render() {
+  // 	console.log('props', this.props);
+  // 	return (
+  // 		<article className="Article">
+  // 			article
+  // 		</article>
+  // 	)
+  componentDidMount() {
+    // this.fetchData();
+    // let articles = this.state.articles;
+    let articles = [
+      "<p>Filler text because we don't consistently have data......  Lorem ipsum, blah, blah, blah.....</p>",
+    ];
+    console.log('this.state.articles', articles);
+    if (articles.length) {
+      let articleSpace = document.getElementById('article-space');
+      articles.forEach((article, i) => {
+        // console.log("count", i)
+        let d = document.createElement('div');
+        d.id = `article-{$i}`;
+        d.innerHTML = article;
+        articleSpace.appendChild(d);
+      });
+    }
+  }
+  render() {
+    let { article } = this.props;
+    return (
+      <article className="Article">
+        <h2>{article.title}</h2>
+        <p>{article.description}</p>
+        <div id="article-space" />
+      </article>
+    );
+  }
 }
+
+// const Article = (props) => {
+// }
 
 export default Article;
