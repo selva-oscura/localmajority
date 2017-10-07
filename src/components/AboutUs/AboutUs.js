6f8939d74a2bdc9c685b7b6a2c9c981b74a70d78
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:39:03 -0700

    linting (no changes to substance)

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -2,32 +2,137 @@ import React from 'react';
 import './AboutUs.css';
 
 const AboutUs = () => {
-	return (
-		<article className="AboutUs">
-			<iframe src="https://player.vimeo.com/video/220517251" width="640" height="360" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
-			<h2>LOCAL MAJORITY IS A NEW POLITICAL ACTION COMMITTEE</h2>
-			<h3>With the federal government firmly in the hands of the right wing, the front line in the battle to advance Democratic priorities in America has shifted to key state capitols.<br />Progressive victories in 2018 and beyond will depend on the immediate implementation of robust state-level efforts combining grass-roots activism with strategic investments in high-potential candidates and races.</h3>
-			<h3>The right wing is already there. Local Majority plans to fight fire with fire.</h3>
-			<h2>RED AND PURPLE STATES MATTER</h2>
-			<h3>In some ways, the outcome of the November 2016 election may have been inevitable. Focused on national policy over the past several decades, progressives—who tend to be concentrated in a handful of politically friendly regions in coastal states—largely ceded power in many states to the right wing as it took up residence under the Republican banner.</h3>
-			<h3>The result is that 25 American states are now governed by a Republican majority or supermajority in which the right wing controls the governorship and both houses of the state legislature, sometimes with a two-thirds majority that allows for implementation of an extremist agenda with near impunity. In a number of cases, this ideology also extends to the state supreme court.</h3>
-			<h3>This is increasingly becoming a human rights issue. Having lost their protection in Washington, our brothers and sisters in Republican-controlled states who seek basic human rights such as equal protection under the law, affordable health care, environmental justice, and the right to control their own reproduction are today more vulnerable than ever. These basic rights are under attack right now in fully one-half of our country. And the trend is not moving in the favor of liberty. Voter suppression is on the rise. We need to act now to stop this erosion of liberty affecting millions of Americans.</h3>
-			<h2>AS GO THE STATES, SO GOES THE NATION</h2>
-			<h3>How can state assembly races in far-flung districts affect the entire nation? In a word: gerrymandering. Every ten years the publication of the federal census triggers a redrawing of the district boundaries governing a population’s representation in the state government and the U.S. Congress. A majority in the state legislature can further strengthen its position by allowing partisans to control this process. Thus the 25 Republican majorities in statehouses across America have everything to do with the makeup of the U.S. Congress and, by extension, the Supreme Court.</h3>
-			<h3>The bad news is that for a decade or more the deck has been stacked in many conservative states, thanks to the disproportionate influence of state legislators over district boundaries. The good news is that a slow but steady demographic shift in key states is creating new opportunities to effect significant change. Strategic efforts to win back even a handful of seats in targeted state capitols will have an especially positive impact on future elections.</h3>
-			<h3>Census data will next be released in 2020 and leaders in every state will seek to control the makeup of districts for the 2022 elections. We are playing the long game. What we do — or don’t do — in 2017 will be directly felt in 2022.</h3>
-			<h2>NOT JUST ANOTHER POLITICAL ACTION COMMITTEE</h2>
-			<h3>Progressive Americans who would like to see change in 2020 have not just an opportunity but an imperative to act now. Virginia, North Carolina, and a handful of other swing states are emerging as critical battlegrounds in 2017, 2018, and beyond, and the usual suspects are already directing money and attention that way. The ultra-conservative political fundraising network associated with the Koch brothers expects to direct $300-$400 million toward this fight in the next two years alone.</h3>
-			<h2>WE NEED TO FIGHT FIRE WITH FIRE. AND THAT MEANS TRYING SOMETHING NEW.</h2>
-			<h3>Local Majority brings the best of all worlds to this fight. Its principals are veteran Democratic operatives who have close ties to key individuals and organizations at the forefront of the progressive movement but are not subject to their constraints.</h3>
-			<h3>Institutions such as Planned Parenthood, the American Civil Liberties Union, and the Democratic National Committee are doing important work, but much of it is being carried out in silos.</h3>
-			<h3>Local Majority is uniquely positioned to identify and fill the gaps between these silos in a way that complements and does not compete with their efforts. As a political action committee, it is to the established progressive institutions as special operations are to the armed forces. Nimble and pragmatic, Local Majority can deploy assets strategically to enhance the entire effort.</h3>
-			<h2>EMPOWERING THE LOCAL MAJORITY</h2>
-			<h3>&ldquo;All politics is local,&rdquo; former Speaker of the U.S. House of Representatives Tip O’Neill once famously said.</h3>
-			<h3>Hard-won environmental and civil rights protections, the makeup of the United States Supreme Court, indeed the very heart and soul of our nation—this is what is at stake. But the fight will only be won by diving deep into local contests, district by district and state by state.</h3>
-			<h3>At this very moment, the torch is being carried by hard-working activists working tirelessly to elect candidates we have never heard of but on whom our aspirations in 2020 and beyond may depend. Their opposition is already flush with right-wing money.</h3>
-		</article>
-	);
+  return (
+    <article className="AboutUs">
+      <iframe
+        src="https://player.vimeo.com/video/220517251"
+        width="640"
+        height="360"
+        frameborder="0"
+        webkitallowfullscreen=""
+        mozallowfullscreen=""
+        allowfullscreen=""
+      />
+      <h2>LOCAL MAJORITY IS A NEW POLITICAL ACTION COMMITTEE</h2>
+      <h3>
+        With the federal government firmly in the hands of the right wing, the
+        front line in the battle to advance Democratic priorities in America has
+        shifted to key state capitols.<br />Progressive victories in 2018 and
+        beyond will depend on the immediate implementation of robust state-level
+        efforts combining grass-roots activism with strategic investments in
+        high-potential candidates and races.
+      </h3>
+      <h3>
+        The right wing is already there. Local Majority plans to fight fire with
+        fire.
+      </h3>
+      <h2>RED AND PURPLE STATES MATTER</h2>
+      <h3>
+        In some ways, the outcome of the November 2016 election may have been
+        inevitable. Focused on national policy over the past several decades,
+        progressives—who tend to be concentrated in a handful of politically
+        friendly regions in coastal states—largely ceded power in many states to
+        the right wing as it took up residence under the Republican banner.
+      </h3>
+      <h3>
+        The result is that 25 American states are now governed by a Republican
+        majority or supermajority in which the right wing controls the
+        governorship and both houses of the state legislature, sometimes with a
+        two-thirds majority that allows for implementation of an extremist
+        agenda with near impunity. In a number of cases, this ideology also
+        extends to the state supreme court.
+      </h3>
+      <h3>
+        This is increasingly becoming a human rights issue. Having lost their
+        protection in Washington, our brothers and sisters in
+        Republican-controlled states who seek basic human rights such as equal
+        protection under the law, affordable health care, environmental justice,
+        and the right to control their own reproduction are today more
+        vulnerable than ever. These basic rights are under attack right now in
+        fully one-half of our country. And the trend is not moving in the favor
+        of liberty. Voter suppression is on the rise. We need to act now to stop
+        this erosion of liberty affecting millions of Americans.
+      </h3>
+      <h2>AS GO THE STATES, SO GOES THE NATION</h2>
+      <h3>
+        How can state assembly races in far-flung districts affect the entire
+        nation? In a word: gerrymandering. Every ten years the publication of
+        the federal census triggers a redrawing of the district boundaries
+        governing a population’s representation in the state government and the
+        U.S. Congress. A majority in the state legislature can further
+        strengthen its position by allowing partisans to control this process.
+        Thus the 25 Republican majorities in statehouses across America have
+        everything to do with the makeup of the U.S. Congress and, by extension,
+        the Supreme Court.
+      </h3>
+      <h3>
+        The bad news is that for a decade or more the deck has been stacked in
+        many conservative states, thanks to the disproportionate influence of
+        state legislators over district boundaries. The good news is that a slow
+        but steady demographic shift in key states is creating new opportunities
+        to effect significant change. Strategic efforts to win back even a
+        handful of seats in targeted state capitols will have an especially
+        positive impact on future elections.
+      </h3>
+      <h3>
+        Census data will next be released in 2020 and leaders in every state
+        will seek to control the makeup of districts for the 2022 elections. We
+        are playing the long game. What we do — or don’t do — in 2017 will be
+        directly felt in 2022.
+      </h3>
+      <h2>NOT JUST ANOTHER POLITICAL ACTION COMMITTEE</h2>
+      <h3>
+        Progressive Americans who would like to see change in 2020 have not just
+        an opportunity but an imperative to act now. Virginia, North Carolina,
+        and a handful of other swing states are emerging as critical
+        battlegrounds in 2017, 2018, and beyond, and the usual suspects are
+        already directing money and attention that way. The ultra-conservative
+        political fundraising network associated with the Koch brothers expects
+        to direct $300-$400 million toward this fight in the next two years
+        alone.
+      </h3>
+      <h2>
+        WE NEED TO FIGHT FIRE WITH FIRE. AND THAT MEANS TRYING SOMETHING NEW.
+      </h2>
+      <h3>
+        Local Majority brings the best of all worlds to this fight. Its
+        principals are veteran Democratic operatives who have close ties to key
+        individuals and organizations at the forefront of the progressive
+        movement but are not subject to their constraints.
+      </h3>
+      <h3>
+        Institutions such as Planned Parenthood, the American Civil Liberties
+        Union, and the Democratic National Committee are doing important work,
+        but much of it is being carried out in silos.
+      </h3>
+      <h3>
+        Local Majority is uniquely positioned to identify and fill the gaps
+        between these silos in a way that complements and does not compete with
+        their efforts. As a political action committee, it is to the established
+        progressive institutions as special operations are to the armed forces.
+        Nimble and pragmatic, Local Majority can deploy assets strategically to
+        enhance the entire effort.
+      </h3>
+      <h2>EMPOWERING THE LOCAL MAJORITY</h2>
+      <h3>
+        &ldquo;All politics is local,&rdquo; former Speaker of the U.S. House of
+        Representatives Tip O’Neill once famously said.
+      </h3>
+      <h3>
+        Hard-won environmental and civil rights protections, the makeup of the
+        United States Supreme Court, indeed the very heart and soul of our
+        nation—this is what is at stake. But the fight will only be won by
+        diving deep into local contests, district by district and state by
+        state.
+      </h3>
+      <h3>
+        At this very moment, the torch is being carried by hard-working
+        activists working tirelessly to elect candidates we have never heard of
+        but on whom our aspirations in 2020 and beyond may depend. Their
+        opposition is already flush with right-wing money.
+      </h3>
+    </article>
+  );
 };
 
 export default AboutUs;
