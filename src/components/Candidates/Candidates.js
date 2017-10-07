fe0cd8b4da611989c662d8c7b17a8a324100d322
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-06 20:23:10 -0700

    added switching between CandidateCard and Loading stub when data is loading

--- src/components/Candidates/Candidates.js
+++ src/components/Candidates/Candidates.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import CandidateCard from './CandidateCard';
 
-const Candidates = (props) => {
-	console.log('props for Candidates', props);
+const Candidates = props => {
+  console.log('props for Candidates', props);
   const candidates = props.candidates;
-  document.title = "Local Majority | Candidates";
+  document.title = 'Local Majority | Candidates';
   return (
     <div className="Candidates flex">
-    	{candidates.map((candidate, i) => (
-				<CandidateCard
-	    		key={i}
-	    		title={candidate.title}
-	    		subtitle={candidate.district}
-	    		text={candidate.summary}
-	    		imgSrc={candidate.imgSrc}
-	    		category="candidates"
-	    		id={candidate.id}
-	    		imgShape="square"
-	    	/>
-    	))}
+      {candidates.length ? (
+        candidates.map((candidate, i) => {
+          let headshot =
+            candidate.headshotSm && candidate.headshotSm.url
+              ? candidate.headshotSm.url
+              : 'missing image';
+          return (
+            <CandidateCard
+              key={i}
+              title={candidate.title}
+              imgSrc={headshot}
+              category="candidates"
+              friendlyId={candidate.friendlyId}
+              imgShape="square"
+            />
+          );
+        })
+      ) : (
+        <h2>Loading</h2>
+      )}
     </div>
   );
 };
