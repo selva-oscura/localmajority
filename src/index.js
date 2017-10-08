86098a6187bdb0661aba8ab13e2f9fb656fda678
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-08 14:42:53 -0700

    added bootstrap-hidden-extract.css to imports; switched MuiThemeProvider, getMuiTheme and localMajorityTheme to material-ui 0.19.x standards

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './bootstrap-reboot.css';
 import './bootstrap-grid.css';
+import './boostrap-hidden-extract.css';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import localMajorityTheme from './configs/localMajorityTheme';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme(localMajorityTheme)}>
+      <App />
+    </MuiThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
