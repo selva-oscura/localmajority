80189b53a74d9820f4626efb0b0d22ee720aa4ff
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-09 22:43:25 -0700

    created handleDrawerClose; streamlined handleHamburgerClick; added label to AppBar 'hamburger'; added docked, width, open, and onRequestChange arameters to Drawer; linting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,75 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-
+import { AppBar, Drawer, IconButton } from 'material-ui';
 import NavBar from './NavBar';
 // import './Header.css';
 
-class Header extends Component{
+class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
-    this.handleHamburgerClick = this.handleHamburgerClick.bind(this);
+    this.state = { open: false };
   }
-  handleHamburgerClick(){
-    console.log('clicked!');
-  }
-  render(){
-	  const navLinks = [
-	    { path: '/', name: 'Home', shortName: 'Home', exact: true },
-	    {
-	      path: '/candidates',
-	      name: 'Our Candidates',
-	      shortName: 'Candidates',
-	      exact: false,
-	    },
-	    {
-	      path: '/districts',
-	      name: 'Our Districts',
-	      shortName: 'Districts',
-	      exact: false,
-	    },
-	    { path: '/issues', name: 'Issues', shortName: 'Issues', exact: false },
-	    { path: '/about-us', name: 'About Us', shortName: 'About Us', exact: true },
-	  ];
-	  return (
-	    <header className="Header">
-	      <div className="hidden-md-up">
-	        <AppBar
-	          position="static"
-	          color="primary"
-	          title={<span>Local Majority</span>}
-	          onLeftIconButtonTouchTap={this.handleHamburgerClick}
-	        >
-	        </AppBar>
-	        <Drawer>
+  handleHamburgerClick = () => this.setState({ open: !this.state.open });
+  handleDrawerClose = () => this.setState({ open: false });
+  render() {
+    const navLinks = [
+      { path: '/', name: 'Home', shortName: 'Home', exact: true },
+      {
+        path: '/candidates',
+        name: 'Our Candidates',
+        shortName: 'Candidates',
+        exact: false,
+      },
+      {
+        path: '/districts',
+        name: 'Our Districts',
+        shortName: 'Districts',
+        exact: false,
+      },
+      { path: '/issues', name: 'Issues', shortName: 'Issues', exact: false },
+      {
+        path: '/about-us',
+        name: 'About Us',
+        shortName: 'About Us',
+        exact: true,
+      },
+    ];
+    return (
+      <header className="Header">
+        <div className="hidden-md-up">
+          <AppBar
+            position="static"
+            color="primary"
+            title={<span>Local Majority</span>}
+            menuElementLeft={<IconButton label={'Open Navigation Menu'} />}
+            onLeftIconButtonTouchTap={this.handleHamburgerClick}
+          />
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
             {navLinks.map((link, i) => (
-            	<h3 key={i}>
-							  <NavLink
-							    to={link.path}
-							    exact={link.exact}
-							    activeClassName="selected"
-							  >
-							    {link.name}
-							  </NavLink>
-						  </h3>
+              <h3 key={i}>
+                <NavLink
+                  to={link.path}
+                  exact={link.exact}
+                  activeClassName="selected"
+                  onClick={this.handleDrawerClose}
+                >
+                  {link.name}
+                </NavLink>
+              </h3>
             ))}
-	        </Drawer>
-	      </div>
-	      <div className="hidden-sm-down">
-		      <NavBar 
-		      	navLinks={navLinks}
-		      />
-		    </div>
-	    </header>
-	  );
+          </Drawer>
+        </div>
+        <div className="hidden-sm-down">
+          <NavBar navLinks={navLinks} />
+        </div>
+      </header>
+    );
   }
-
 }
 
 export default Header;
