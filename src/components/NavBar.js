022b8c3474ee4776dd4be19e3b4bcd47cebf0379
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-09 22:44:18 -0700

    linting (tabs -> spaces)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = ({navLinks}) => {
+const NavBar = ({ navLinks }) => {
   return (
-    <nav className="NavBar">
-      {navLinks.map((link, i) => (
-        <NavLink
-          key={i}
-          to={link.path}
-          exact={link.exact}
-          activeClassName="selected"
-        >
-          {link.name}
-        </NavLink>
-      ))}
-    </nav>
+    <div className="NavBar">
+      <h1>Local Majority</h1>
+      <nav>
+        {navLinks.map((link, i) => (
+          <NavLink
+            key={i}
+            to={link.path}
+            exact={link.exact}
+            activeClassName="selected"
+          >
+            {link.name}
+          </NavLink>
+        ))}
+      </nav>
+    </div>
   );
 };
 
