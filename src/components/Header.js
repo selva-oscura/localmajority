86177816ee377a53afb8c0c08688c37d9a2f811b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-09 18:26:35 -0700

    converted Header to stateful component; defined navLinksand passed to NavBar;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import Nav from './Nav';
-import './Header.css';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
 
-const Header = () => {
-  return (
-    <header className="Header">
-      <h1>Local Majority</h1>
-      <Nav />
-    </header>
-  );
-};
+import NavBar from './NavBar';
+// import './Header.css';
+
+class Header extends Component{
+  render(){
+	  const navLinks = [
+	    { path: '/', name: 'Home', shortName: 'Home', exact: true },
+	    {
+	      path: '/candidates',
+	      name: 'Our Candidates',
+	      shortName: 'Candidates',
+	      exact: false,
+	    },
+	    {
+	      path: '/districts',
+	      name: 'Our Districts',
+	      shortName: 'Districts',
+	      exact: false,
+	    },
+	    { path: '/issues', name: 'Issues', shortName: 'Issues', exact: false },
+	    { path: '/about-us', name: 'About Us', shortName: 'About Us', exact: true },
+	  ];
+	  return (
+	    <header className="Header">
+	      <div className="hidden-md-up">
+	      </div>
+	      <div className="hidden-sm-down">
+		      <NavBar 
+		      	navLinks={navLinks}
+		      />
+		    </div>
+	    </header>
+	  );
+  }
+
+}
 
 export default Header;
