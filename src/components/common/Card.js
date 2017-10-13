eded0de23b357fd9a7c7b127311040295300661c
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-12 19:44:25 -0700

    reverted to non-css/component-library Card

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Card.css';
 
-const Card = ({ title, subtitle, text, imgSrc, category, id, imgShape }) => {
+const Card = ({ id, cardTitle, cardSubtitle, cardText, imgSrc, category, friendlyId, imgShape }) => {
   let aspectRatio = imgShape || '';
   return (
     <div className={`Card ${aspectRatio}`}>
-      <Link to={`/${category}/${id}`}>
+      <Link to={`/${category}/${friendlyId}`}>
         <div className="image-holder">
           <img
             className="placeholder-image"
-            alt={`placeholder for ${title}`}
+            alt={`placeholder for ${cardTitle}`}
             src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23CCC%22%20d%3D%22M21%2019V5c0-1.1-.9-2-2-2H5c-1.1%200-2%20.9-2%202v14c0%201.1.9%202%202%202h14c1.1%200%202-.9%202-2zM8.5%2013.5l2.5%203.01L14.5%2012l4.5%206H5l3.5-4.5z%22%2F%3E%3C%2Fsvg%3E"
           />
-          <img className="actual-image" src={`./${imgSrc}`} alt={title} />
+          <img className="actual-image" src={`./${imgSrc}`} alt={cardTitle} />
         </div>
-        <h3>{title}</h3>
-        {subtitle && <h4>{subtitle}</h4>}
-        <p>{text}</p>
+        <h3>{cardTitle}</h3>
+        {cardSubtitle && <h4>{cardSubtitle}</h4>}
+        <p>{cardText}</p>
       </Link>
     </div>
   );
@@ -27,11 +27,13 @@ const Card = ({ title, subtitle, text, imgSrc, category, id, imgShape }) => {
 Card.propTypes = {
   id: PropTypes.string,
   category: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string,
-  text: PropTypes.string.isRequired,
+  cardTitle: PropTypes.string,
+  cardSubtitle: PropTypes.string,
+  cardText: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
-  imgShape: PropTypes.string.isRequired,
 };
 
 export default Card;
+
+// imgShape: PropTypes.string.isRequired,
+// cardTitle, subtitle, imgSrc, cardText 
