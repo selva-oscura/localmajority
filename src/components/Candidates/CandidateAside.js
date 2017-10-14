6dd74154d0e4c1cb5f1a57200c308e07aa19803f
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-14 15:32:21 -0700

    added donate to candidate RaisedButton; minor syntax changes;

--- src/components/Candidates/CandidateAside.js
+++ src/components/Candidates/CandidateAside.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../common/Aside.css';
 import TwitterTimeline from '../common/TwitterTimeline';
 import { SocialIcon } from 'react-social-icons';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const CandidateAside = props => {
   console.log('props from Aside', props);
@@ -12,15 +13,19 @@ const CandidateAside = props => {
       <TwitterTimeline twitterHandle={[candidate.twitter]} />
       <h3>Contact the Candidate</h3>
       <div className="row">
-        {candidate.twitter ? (
-          <SocialIcon url={`https://twitter.com/${candidate.twitter}`} />
-        ) : null}
-        {candidate.facebook ? (
-          <SocialIcon url={`https://www.facebook.com/${candidate.facebook}`} />
-        ) : null}
-        {candidate.email ? (
-          <SocialIcon url={`mailto:${candidate.email}`} network="email" />
-        ) : null}
+        { candidate.twitter && <SocialIcon url={`https://twitter.com/${candidate.twitter}`} /> }
+        { candidate.facebook && <SocialIcon url={`https://www.facebook.com/${candidate.facebook}`} /> }
+        { candidate.campaignEmail && <SocialIcon url={`mailto:${candidate.campaignEmail}`} network="email" /> }
+      </div>
+      <div className="row">
+        { candidate.donateUrl &&
+          <RaisedButton
+            secondary={true}
+            label="Donate"
+            href={candidate.donateUrl}
+          >
+          </RaisedButton>
+        }
       </div>
     </aside>
   );
