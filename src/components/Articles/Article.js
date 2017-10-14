365294c1e10f859051595232ec561e1aac27d6d7
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-14 13:40:48 -0700

    changed appending of data to pull article.bodyText; added missing author note; removed some commented-out/deprecated code;

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -2,44 +2,51 @@
 import React, { Component } from 'react';
 
 class Article extends Component {
-  // render() {
-  // 	console.log('props', this.props);
-  // 	return (
-  // 		<article className="Article">
-  // 			article
-  // 		</article>
-  // 	)
   componentDidMount() {
+    let { article } = this.props;
+    let articleSpace = document.getElementById('article-body');
+    if (article.bodyText) {
+      let d = document.createElement('div');
+      d.id = 'article-{$i}';
+      d.innerHTML = article.bodyText;
+      articleSpace.appendChild(d);
+    }
+
     // this.fetchData();
     // let articles = this.state.articles;
-    let articles = [
-      "<p>Filler text because we don't consistently have data......  Lorem ipsum, blah, blah, blah.....</p>",
-    ];
-    console.log('this.state.articles', articles);
-    if (articles.length) {
-      let articleSpace = document.getElementById('article-space');
-      articles.forEach((article, i) => {
-        // console.log("count", i)
-        let d = document.createElement('div');
-        d.id = `article-{$i}`;
-        d.innerHTML = article;
-        articleSpace.appendChild(d);
-      });
-    }
+
+    // let articles = [
+    //   "<p>Filler text because we don't consistently have data......  Lorem ipsum, blah, blah, blah.....</p>",
+    // ];
+    // console.log('this.state.articles', articles);
+    // if (articles.length) {
+    //   let articleSpace = document.getElementById('article-space');
+    //   articles.forEach((article, i) => {
+    //     // console.log("count", i)
+    //     let d = document.createElement('div');
+    //     d.id = `article-{$i}`;
+    //     d.innerHTML = article;
+    //     articleSpace.appendChild(d);
+    //   });
+    // }
   }
   render() {
     let { article } = this.props;
     return (
       <article className="Article">
         <h2>{article.title}</h2>
+        {article.author ? (
+          <h3>by {article.author}</h3>
+        ): (
+          <h3>MISSING AUTHOR INFORMATION</h3>
+        )
+        }
+        <h4>Last Updated: {article.created.slice(0,10)}</h4>
         <p>{article.description}</p>
-        <div id="article-space" />
+        <div id="article-body" />
       </article>
     );
   }
 }
-
-// const Article = (props) => {
-// }
 
 export default Article;
