3e2fcc830fb817a5aeb15da461e9542b901dd534
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-14 17:12:24 -0700

    created TalkingPoint component (takes string title and html body)

---
+++ src/components/common/TalkingPoint.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class TalkingPoint extends Component {
+  componentDidMount(){
+    let { body } = this.props.talkingPoint;
+    let tpBodySpace = document.getElementsByClassName('tpBody')[0];
+    if (body) {
+      let d = document.createElement('div');
+      d.innerHTML = body;
+      tpBodySpace.appendChild(d);
+    }
+  }
+  render(){
+    let { title } = this.props.talkingPoint;
+    return (
+      <div className="TalkingPoint">
+        <h2>{title}</h2>
+        <div className="tpBody"></div>
+      </div>
+    );
+  }
+}
+
+export default TalkingPoint;
