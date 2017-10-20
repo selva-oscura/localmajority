2ce19ff78b7ecf6e2d6be25cbe2b862e200effb1
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-19 22:38:41 -0700

    created DistrictPrimer and TalkingPoints components (forked from Article, will need retailoring to match the data)

---
+++ src/components/Readings/TalkingPoints.js
@@ -0,0 +1,51 @@
+// import React from 'react';
+import React, { Component } from 'react';
+
+class TalkingPoints extends Component {
+  componentDidMount() {
+    let { reading } = this.props;
+    let readingSpace = document.getElementById('reading-body');
+    if (reading.bodyText) {
+      let d = document.createElement('div');
+      d.id = 'reading-{$i}';
+      d.innerHTML = reading.bodyText;
+      readingSpace.appendChild(d);
+    }
+
+    // this.fetchData();
+    // let articles = this.state.articles;
+
+    // let articles = [
+    //   "<p>Filler text because we don't consistently have data......  Lorem ipsum, blah, blah, blah.....</p>",
+    // ];
+    // console.log('this.state.articles', articles);
+    // if (articles.length) {
+    //   let articleSpace = document.getElementById('article-space');
+    //   articles.forEach((article, i) => {
+    //     // console.log("count", i)
+    //     let d = document.createElement('div');
+    //     d.id = `article-{$i}`;
+    //     d.innerHTML = article;
+    //     articleSpace.appendChild(d);
+    //   });
+    // }
+  }
+  render() {
+    let { title, author, description, created } = this.props.reading;
+    return (
+      <article className="TalkingPoints">
+        <h2>{title}</h2>
+        {author ? (
+          <h3>by {author}</h3>
+        ) : (
+          <h3>MISSING AUTHOR INFORMATION</h3>
+        )}
+        <h4>Last Updated: {created.slice(0, 10)}</h4>
+        <p>{description}</p>
+        <div id="reading-body" />
+      </article>
+    );
+  }
+}
+
+export default TalkingPoints;
