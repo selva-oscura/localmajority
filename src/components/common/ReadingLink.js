79934fadc6863d7876619f199e4311e46579b815
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-21 01:09:10 -0700

    created ReadingLink (display Articles, IssuePrimer, TalkingPoints, DistrictPrimer links)

---
+++ src/components/common/ReadingLink.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ReadingLink = ({ friendlyId, title, type }) => {
+  const readingTypesText = {
+    TalkingPoints: 'Talking Points',
+    Articles: 'Article',
+    DistrictPrimers: 'District Primer',
+  };
+  const readingTypesRoute = {};
+  const readingTypeText = readingTypesText[type];
+  return (
+    <Link className="ReadingLink" key={i} to={`/${type}/${friendlyId}`}>
+      <h4>
+        <span className="reading-type">{readingTypeText}</span>
+        <span className="reading-title">{title}</span>
+      </h4>
+    </Link>
+  );
+};
+
+export default ReadingLink;
