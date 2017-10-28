15affe39de85f125c54e27085324f5a65d2943b3
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-28 16:26:49 -0700

    streamlined display of districtPrimer text in DistrictPrimer; removed deprecated content, notes re: data structure;

--- src/components/Readings/DistrictPrimer.js
+++ src/components/Readings/DistrictPrimer.js
@@ -4,46 +4,32 @@ import React, { Component } from 'react';
 class DistrictPrimer extends Component {
   componentDidMount() {
     let { reading } = this.props;
-    console.log('districtPrimer', reading);
-    let readingBody = document.getElementById('reading-body');
-    if (reading.bodyText) {
-      let d = document.createElement('div');
-      d.id = 'reading-{$i}';
-      d.innerHTML = reading.bodyText;
-      readingBody.appendChild(d);
-    }
-
-    // this.fetchData();
-    // let articles = this.state.articles;
-
-    // let articles = [
-    //   "<p>Filler text because we don't consistently have data......  Lorem ipsum, blah, blah, blah.....</p>",
-    // ];
-    // console.log('this.state.articles', articles);
-    // if (articles.length) {
-    //   let articleSpace = document.getElementById('article-space');
-    //   articles.forEach((article, i) => {
-    //     // console.log("count", i)
-    //     let d = document.createElement('div');
-    //     d.id = `article-{$i}`;
-    //     d.innerHTML = article;
-    //     articleSpace.appendChild(d);
-    //   });
-    // }
+    let details = [
+      'knowTheDistrict',
+      'facts',
+      'summary',
+      'howWeWin',
+      'aboutOpposition',
+      'bodyText',
+    ];
+    let districtPrimerSpace = document.getElementById('district-primer-space');
+    details.forEach(detail => {
+      if (reading[detail]) {
+        let d = document.createElement('div');
+        d.id=detail;
+        d.innerHTML = reading[detail];
+        districtPrimerSpace.appendChild(d);
+      }
+    })
   }
   render() {
-    // let { title, author, description, created } = this.props.reading;
-    // console.log('reading', reading);
     return (
       <article className="DistrictPrimer">
-        <div id="reading-body" />
+        <h3>About the District</h3>
+        <div id="district-primer-space" />
       </article>
     );
   }
 }
 
 export default DistrictPrimer;
-// <h2>{title}</h2>
-// {author ? <h3>by {author}</h3> : <h3>MISSING AUTHOR INFORMATION</h3>}
-// <h4>Last Updated: {created.slice(0, 10)}</h4>
-// <p>{description}</p>
