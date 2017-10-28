6376b64d662ac63f9db8465a90288194c71f0e8c
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-10-28 12:51:07 -0700

    district(s) data renamed to seat(s)

--- src/components/Districts/Districts.js
+++ src/components/Districts/Districts.js
@@ -3,22 +3,22 @@ import React from 'react';
 import Card from '../common/Card';
 
 const Districts = props => {
-  const districts = props.districts;
+  const seats = props.seats;
   console.log('props for Districts', props);
   document.title = 'Local Majority | Districts';
   //  console.log('districts', districts);
   return (
     <div className="Districts flex">
-      {districts.map((district, i) => (
+      {seats.map((seat, i) => (
         <Card
           key={i}
-          id={district.id}
-          cardTitle={district.title}
+          id={seat.uid}
+          cardTitle={seat.title}
           cardText="need to add once we have data"
           imgSrc="need to add once we have data"
           category="districts"
-          friendlyId="need to add once we have data"
-          imgShape="landscape"
+          friendlyId={seat.friendlyId}
+          imgShape="square"
         />
       ))}
     </div>
