603d5edaaf9b340edc239d8ebd6b55917c728846
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-09 21:51:17 -0800

    added Filters & ButtonFilters display to Districts page, added filtering of seats based upon selected filters

--- src/components/Districts/Districts.js
+++ src/components/Districts/Districts.js
@@ -1,26 +1,39 @@
 import React from 'react';
 // import districts from '../../data/districts.json';
+import Filters from '../Filters/Filters';
+import ButtonFilters from '../Filters/ButtonFilters';
 import Card from '../common/Card';
 
 const Districts = props => {
-  const seats = props.seats;
-  console.log('props for Districts', props);
+  const { seats, statesMasterList, districtsStatesSelected, districtsSeatTypeSelected, updateFilter } = props;
   document.title = 'Local Majority | Districts';
+  let seatsInSelectedStates = seats.filter(seat => districtsStatesSelected[seat.stateName])
   //  console.log('districts', districts);
   return (
-    <div className="Districts flex">
-      {seats.map((seat, i) => (
-        <Card
-          key={i}
-          id={seat.uid}
-          cardTitle={seat.title}
-          cardText={seat.candidateName}
-          imgSrc={seat.mapSmUrl}
-          category="districts"
-          friendlyId={seat.friendlyId}
-          imgShape="square"
+    <div className="Districts">
+      <Filters>
+        <ButtonFilters
+          filterCategory="districtsStatesSelected"
+          includeAllNone={true}
+          masterList={statesMasterList}
+          filterItems={districtsStatesSelected}
+          updateFilter={updateFilter}
         />
-      ))}
+      </Filters>
+      <div className="flex">
+        {seatsInSelectedStates.map((seat, i) => (
+          <Card
+            key={i}
+            id={seat.uid}
+            cardTitle={seat.title}
+            cardText={seat.candidateName}
+            imgSrc={seat.mapSmUrl}
+            category="districts"
+            friendlyId={seat.friendlyId}
+            imgShape="square"
+          />
+        ))}
+      </div>
     </div>
   );
 };
