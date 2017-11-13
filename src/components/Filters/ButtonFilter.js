212b35bc2b989e9e2fca16398471ba025e584e86
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-12 18:28:54 -0800

    increased button spacing (const style); automated linting by prettier

--- src/components/Filters/ButtonFilter.js
+++ src/components/Filters/ButtonFilter.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { RaisedButton } from 'material-ui';
 
-const ButtonFilter = ({ filterCategory, currentItem, filterItems, updateFilter }) => {
-
+const ButtonFilter = ({
+  filterCategory,
+  currentItem,
+  filterItems,
+  updateFilter,
+}) => {
   const handleClick = () => updateFilter(filterCategory, currentItem);
-  const style = { margin: 4 };
-  const [isPrimary, isDefault] = 
-  	filterItems[currentItem] 
-  	|| (currentItem === "none" && Object.entries(filterItems).reduce((sum, item) => (sum + item[1]), 0) === 0) 
-  	|| (currentItem==="all" && Object.entries(filterItems).reduce((sum, item) => (sum + item[1]), 0) === Object.entries(filterItems).length) 
-  	? [true, false] 
-  	: [false, true];
-  
+  const style = { margin: 6 };
+  const [isPrimary, isDefault] =
+    filterItems[currentItem] ||
+    (currentItem === 'none' &&
+      Object.entries(filterItems).reduce((sum, item) => sum + item[1], 0) ===
+        0) ||
+    (currentItem === 'all' &&
+      Object.entries(filterItems).reduce((sum, item) => sum + item[1], 0) ===
+        Object.entries(filterItems).length)
+      ? [true, false]
+      : [false, true];
+
   return (
     <RaisedButton
-    	className='ButtonFilter'
-    	primary={isPrimary}
-    	default={isDefault}
-    	label={currentItem}
-    	style={style}
-    	onClick={handleClick}
+      className="ButtonFilter"
+      primary={isPrimary}
+      default={isDefault}
+      label={currentItem}
+      style={style}
+      onClick={handleClick}
     />
   );
 };
