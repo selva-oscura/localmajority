9412a7389f9a60f12730bf89a83a117b01a44687
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-13 21:50:27 -0800

    made Candidates stateful; imported, passed data to and displayed Filters, ButtonFilters, SelectMultiFilter; moved Candidates' filters (candidatesStatesSelected and candidatesSeatTypeSelected) from App's state to Candidates' state; removed filter props from ButtonFilters, SelectMultiFilter (prevent re-rendering of SelectMultiFilter on state change of filters); added filtering of candidates on page; auto-linting by prettier.js;

--- src/components/Candidates/Candidates.js
+++ src/components/Candidates/Candidates.js
@@ -1,41 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Filters from '../Filters/Filters';
+import ButtonFilters from '../Filters/ButtonFilters';
+import SelectMultiFilter from '../Filters/SelectMultiFilter';
 import Card from '../common/Card';
 
-const Candidates = ({
-  candidates,
-  statesMasterList,
-  candidatesStatesSelected,
-  candidateSeatTypesSelected,
-  candidatesTextSelected,
-}) => {
-  document.title = 'Local Majority | Candidates';
-  return (
-    <div className="Candidates flex">
-      {candidates.length ? (
-        candidates.map((candidate, i) => {
-          let headshotUrl;
-          headshotUrl = candidate.headshotSmUrl
-            ? candidate.headshotSmUrl
-            : candidate.headshotLgUrl;
-          return (
-            <Card
-              key={i}
-              id={candidate.id}
-              cardTitle={candidate.title}
-              cardSubtitle={candidate.seatName}
-              cardText={candidate.introLinkText}
-              category="candidates"
-              imgSrc={headshotUrl}
-              friendlyId={candidate.friendlyId}
-              imgShape="square"
-            />
-          );
-        })
-      ) : (
-        <h2>Loading</h2>
-      )}
-    </div>
-  );
-};
+class Candidates extends Component {
+  constructor(props, context) {
+    super(props, context);
+    let candidatesStatesSelected = {},
+      candidatesSeatTypesSelected = {};
+    this.props.statesMasterList.forEach(stateName => {
+      candidatesStatesSelected[stateName] = true;
+    });
+    this.state = {
+      candidatesStatesSelected,
+      candidatesSeatTypesSelected,
+      candidatesTextSelected: [],
+    };
+    this.updateFilter = this.updateFilter.bind(this);
+  }
+  updateFilter(filterCategory, selectedValues) {
+    let state = this.state;
+    state[filterCategory] = { ...selectedValues };
+    this.setState(state);
+  }
+  componentDidMount() {
+    document.title = 'Local Majority | Candidates';
+  }
+  render() {
+    const { candidates, statesMasterList, seatTypesMasterList } = this.props;
+    const {
+      candidatesStatesSelected,
+      candidatesSeatTypesSelected,
+      candidatesTextSelected,
+    } = this.state;
+    let candidatesMeetingFilters = candidates.filter(
+      candidate => candidatesStatesSelected[candidate.stateName]
+    );
+    return (
+      <div className="Candidates">
+        <Filters>
+          <ButtonFilters
+            filterCategory="candidatesStatesSelected"
+            includeAllNone={true}
+            masterList={statesMasterList}
+            updateFilter={this.updateFilter}
+          />
+          <SelectMultiFilter
+            filterCategory="candidatesStatesSelected"
+            hintText="select state(s)"
+            includeAllNone={true}
+            masterList={statesMasterList}
+            updateFilter={this.updateFilter}
+          />
+        </Filters>
+        <div className="flex">
+          {candidatesMeetingFilters.length ? (
+            candidatesMeetingFilters.map((candidate, i) => {
+              let headshotUrl;
+              headshotUrl = candidate.headshotSmUrl
+                ? candidate.headshotSmUrl
+                : candidate.headshotLgUrl;
+              return (
+                <Card
+                  key={i}
+                  id={candidate.id}
+                  cardTitle={candidate.title}
+                  cardSubtitle={candidate.seatName}
+                  cardText={candidate.introLinkText}
+                  category="candidates"
+                  imgSrc={headshotUrl}
+                  friendlyId={candidate.friendlyId}
+                  imgShape="square"
+                />
+              );
+            })
+          ) : (
+            <h2>No Candidates Meet Your Criteria.</h2>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Candidates;
