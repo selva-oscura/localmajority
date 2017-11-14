be72686ae22956ea2b0d4e8f8df475340e1abe45
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-13 21:48:25 -0800

    made Districts stateful; moved Districts' filters (districtsStatesSelected and districtsSeatTypeSelected) from App's state to Districts' state; removed filter props from ButtonFilters, SelectMultiFilter (prevent re-rendering of SelectMultiFilter on state change of filters); auto-linting by prettier.js;

--- src/components/Districts/Districts.js
+++ src/components/Districts/Districts.js
@@ -1,56 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import districts from '../../data/districts.json';
 import Filters from '../Filters/Filters';
 import ButtonFilters from '../Filters/ButtonFilters';
 import SelectMultiFilter from '../Filters/SelectMultiFilter';
 import Card from '../common/Card';
 
-const Districts = ({
-  seats,
-  statesMasterList,
-  districtsStatesSelected,
-  districtsSeatTypeSelected,
-  updateFilter,
-}) => {
-  document.title = 'Local Majority | Districts';
-  let seatsInSelectedStates = seats.filter(
-    seat => districtsStatesSelected[seat.stateName]
-  );
-  return (
-    <div className="Districts">
-      <Filters>
-        <ButtonFilters
-          filterCategory="districtsStatesSelected"
-          includeAllNone={true}
-          masterList={statesMasterList}
-          filterItems={districtsStatesSelected}
-          updateFilter={updateFilter}
-        />
-        <SelectMultiFilter
-          filterCategory="districtsStatesSelected"
-          hintText="select state(s)"
-          includeAllNone={true}
-          masterList={statesMasterList}
-          filterItems={districtsStatesSelected}
-          updateFilter={updateFilter}
-        />
-      </Filters>
-      <div className="flex">
-        {seatsInSelectedStates.map((seat, i) => (
-          <Card
-            key={i}
-            id={seat.uid}
-            cardTitle={seat.title}
-            cardText={seat.candidateName}
-            imgSrc={seat.mapSmUrl}
-            category="districts"
-            friendlyId={seat.friendlyId}
-            imgShape="square"
+class Districts extends Component {
+  constructor(props, context) {
+    super(props, context);
+    let districtsStatesSelected = {},
+      districtsSeatTypeSelected = {};
+    this.props.statesMasterList.forEach(stateName => {
+      districtsStatesSelected[stateName] = true;
+    });
+    this.state = {
+      districtsStatesSelected,
+      districtsSeatTypeSelected,
+    };
+    this.updateFilter = this.updateFilter.bind(this);
+  }
+  updateFilter(filterCategory, selectedValues) {
+    let state = this.state;
+    state[filterCategory] = { ...selectedValues };
+    this.setState(state);
+  }
+  componentDidMount() {
+    document.title = 'Local Majority | Districts';
+  }
+  render() {
+    const { seats, statesMasterList, seatTypesMasterList } = this.props;
+    const { districtsStatesSelected, districtsSeatTypeSelected } = this.state;
+    let seatsMeetingFilters = seats.filter(
+      seat => districtsStatesSelected[seat.stateName]
+    );
+    return (
+      <div className="Districts">
+        <Filters>
+          <ButtonFilters
+            filterCategory="districtsStatesSelected"
+            includeAllNone={true}
+            masterList={statesMasterList}
+            updateFilter={this.updateFilter}
           />
-        ))}
+          <SelectMultiFilter
+            filterCategory="districtsStatesSelected"
+            hintText="select state(s)"
+            includeAllNone={true}
+            masterList={statesMasterList}
+            updateFilter={this.updateFilter}
+          />
+        </Filters>
+        <div className="flex">
+          {seatsMeetingFilters.length ? (
+            seatsMeetingFilters.map((seat, i) => (
+              <Card
+                key={i}
+                id={seat.uid}
+                cardTitle={seat.title}
+                cardText={seat.candidateName}
+                imgSrc={seat.mapSmUrl}
+                category="districts"
+                friendlyId={seat.friendlyId}
+                imgShape="square"
+              />
+            ))
+          ) : (
+            <h2>No Candidates Meet Your Criteria.</h2>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Districts;
