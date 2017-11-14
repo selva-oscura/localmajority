ff34ef7d05ea5ea4b9fee1fe45bdf4ea22f78856
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-13 21:36:39 -0800

    made SelectMultiFilter stateful (for MUI's MultiFilter not to close, component must hold its own selected values in state); component now maintains a selectedValues object in state and uses that value to updateFilter in parent component (e.g. Candidates, Districts) that consumes the filter; auto-linting by prettier;

--- src/components/Filters/SelectMultiFilter.js
+++ src/components/Filters/SelectMultiFilter.js
@@ -1,66 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MenuItem, SelectField } from 'material-ui';
 
-const SelectMultiFilter = ({
-  filterCategory,
-  hintText,
-  includeAllNone,
-  masterList,
-  filterItems,
-  updateFilter,
-}) => {
-  const handleChange = (e, i, clickedItem) =>
-    updateFilter(filterCategory, clickedItem[0]);
-  const selectionRenderer = () => {
-    const selectionArr = Object.entries(filterItems)
-      .filter(filterItem => filterItem[1])
-      .map(filterItem => filterItem[[0]]);
-    return selectionArr.length ? (
-      selectionArr.join(', ')
-    ) : (
-      <span style={{ color: '#007bff' }}>{hintText}</span>
+class SelectMultiFilter extends Component {
+  constructor(props, context) {
+    super(props, context);
+    const selectedValues = {};
+    this.props.masterList.forEach(item => {
+      selectedValues[item] = true;
+    });
+    this.state = { selectedValues };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e, i, clickedItem) {
+    let selectedValues = this.state.selectedValues;
+    if (clickedItem[0] === 'all') {
+      Object.keys(selectedValues).map(key => (selectedValues[key] = true));
+    } else if (clickedItem[0] === 'none') {
+      Object.keys(selectedValues).map(key => (selectedValues[key] = false));
+    } else {
+      selectedValues[clickedItem]
+        ? (selectedValues[clickedItem] = false)
+        : (selectedValues[clickedItem] = true);
+    }
+    this.setState({ selectedValues });
+    this.props.updateFilter(this.props.filterCategory, selectedValues);
+  }
+  render() {
+    const {
+      filterCategory,
+      hintText,
+      includeAllNone,
+      masterList,
+      updateFilter,
+    } = this.props;
+    const menuItems = (masterList, selectedValues) =>
+      masterList.map(item => (
+        <MenuItem
+          key={item}
+          insetChildren={true}
+          checked={selectedValues && selectedValues[item]}
+          value={item}
+          primaryText={item}
+        />
+      ));
+    const selectionRenderer = () => {
+      const selectionArr = Object.entries(this.state.selectedValues)
+        .filter(filterItem => filterItem[1])
+        .map(filterItem => filterItem[[0]]);
+      return selectionArr.length ? (
+        selectionArr.join(', ')
+      ) : (
+        <span style={{ color: '#007bff' }}>{hintText}</span>
+      );
+    };
+    const totalSelected = Object.values(this.state.selectedValues).reduce(
+      (sum, item) => sum + item,
+      0
     );
-  };
-  const menuItems = (masterList, filterItems) => {
-    return masterList.map(item => (
-      <MenuItem
-        key={item}
-        insetChildren={true}
-        checked={filterItems && filterItems[item]}
-        value={item}
-        primaryText={item}
-      />
-    ));
-  };
-  const totalSelected = Object.entries(filterItems).reduce(
-    (sum, item) => sum + item[1],
-    0
-  );
-  return (
-    <SelectField
-      multiple={true}
-      selectionRenderer={selectionRenderer}
-      onChange={handleChange}
-    >
-      {includeAllNone && [
-        <MenuItem
-          key="all"
-          insetChildren={true}
-          checked={totalSelected === masterList.length}
-          value="all"
-          primaryText="All"
-        />,
-        <MenuItem
-          key="none"
-          insetChildren={true}
-          checked={totalSelected === 0}
-          value="none"
-          primaryText="None"
-        />,
-      ]}
-      {menuItems(masterList, filterItems)}
-    </SelectField>
-  );
-};
+    return (
+      <SelectField
+        multiple={true}
+        selectionRenderer={selectionRenderer}
+        onChange={this.handleChange}
+      >
+        {includeAllNone && [
+          <MenuItem
+            key="all"
+            insetChildren={true}
+            checked={totalSelected === masterList.length}
+            value="all"
+            primaryText="All"
+          />,
+          <MenuItem
+            key="none"
+            insetChildren={true}
+            checked={totalSelected === 0}
+            value="none"
+            primaryText="None"
+          />,
+        ]}
+        {menuItems(masterList, this.state.selectedValues)}
+      </SelectField>
+    );
+  }
+}
 
 export default SelectMultiFilter;
