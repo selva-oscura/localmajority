3aeb9a9f36832a6865a07b7c9763f2ef4960ade0
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-13 21:41:04 -0800

    made ButtonFilters stateful to be consistent with SelectMultiFilter (selectedValues held as object in state and updateFilters called on parent component (e.g. Candidates, Districts)); auto-linting with prettier;

--- src/components/Filters/ButtonFilters.js
+++ src/components/Filters/ButtonFilters.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ButtonFilter from './ButtonFilter';
 
-const ButtonFilters = ({
-  filterCategory,
-  includeAllNone,
-  masterList,
-  filterItems,
-  updateFilter,
-}) => (
-  <div className="ButtonFilters flex">
-    {includeAllNone && [
-      <ButtonFilter
-        key="all"
-        filterCategory={filterCategory}
-        currentItem="all"
-        filterItems={filterItems}
-        updateFilter={updateFilter}
-      />,
-      <ButtonFilter
-        key="none"
-        filterCategory={filterCategory}
-        currentItem="none"
-        filterItems={filterItems}
-        updateFilter={updateFilter}
-      />,
-    ]}
-    {masterList.map((item, i) => (
-      <ButtonFilter
-        key={i}
-        filterCategory={filterCategory}
-        currentItem={item}
-        filterItems={filterItems}
-        updateFilter={updateFilter}
-      />
-    ))}
-  </div>
-);
+class ButtonFilters extends Component {
+  constructor(props, context) {
+    super(props, context);
+    const selectedValues = {};
+    this.props.masterList.forEach(item => {
+      selectedValues[item] = true;
+    });
+    this.state = { selectedValues };
+    this.updateSelectedValues = this.updateSelectedValues.bind(this);
+  }
+  updateSelectedValues(filterCategory, currentItem) {
+    let selectedValues = this.state.selectedValues;
+    if (currentItem === 'all') {
+      Object.keys(selectedValues).map(key => (selectedValues[key] = true));
+    } else if (currentItem === 'none') {
+      Object.keys(selectedValues).map(key => (selectedValues[key] = false));
+    } else {
+      selectedValues[currentItem]
+        ? (selectedValues[currentItem] = false)
+        : (selectedValues[currentItem] = true);
+    }
+    this.setState({ selectedValues });
+    this.props.updateFilter(filterCategory, selectedValues);
+  }
+  render() {
+    const {
+      filterCategory,
+      includeAllNone,
+      masterList,
+      filterItems,
+      updateFilter,
+    } = this.props;
+    const { selectedValues } = this.state;
+    return (
+      <div className="ButtonFilters flex">
+        {includeAllNone && [
+          <ButtonFilter
+            key="all"
+            filterCategory={filterCategory}
+            currentItem="all"
+            selectedValues={selectedValues}
+            updateSelectedValues={this.updateSelectedValues}
+          />,
+          <ButtonFilter
+            key="none"
+            filterCategory={filterCategory}
+            currentItem="none"
+            selectedValues={selectedValues}
+            updateSelectedValues={this.updateSelectedValues}
+          />,
+        ]}
+        {masterList.map((item, i) => (
+          <ButtonFilter
+            key={i}
+            filterCategory={filterCategory}
+            currentItem={item}
+            selectedValues={selectedValues}
+            updateSelectedValues={this.updateSelectedValues}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ButtonFilters;
