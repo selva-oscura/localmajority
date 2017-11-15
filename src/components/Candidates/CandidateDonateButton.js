37a21c1160062c97db9740b7c563ac5477dc3f33
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-14 21:59:08 -0800

    created CandidateDonateButton component

---
+++ src/components/Candidates/CandidateDonateButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const CandidateDonateButton = ({candidate}) => {
+  const style = { margin: '1vw 1vw 1vw 0' };
+  const donate = candidate.firstName ? `Donate to ${candidate.firstName}` : "Donate";
+	return (
+    <RaisedButton
+      primary={true}
+      label={donate}
+      href={candidate.donateUrl}
+      style={style}
+      target="donate"
+    />
+	);
+};
+
+export default CandidateDonateButton;
