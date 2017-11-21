20365594cfb36133f7b6a518b4b94bf90026abad
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2017-11-20 18:42:40 -0800

    created HorizontalCards component as div for containing Horizontal Card components

---
+++ src/components/common/Cards/HorizontalCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HorizontalCards.css';
+
+const HorizontalCards = (props) => {
+	return (
+		<div className="HorizontalCards">
+			{props.children}
+		</div>
+	);
+}
+
+export default HorizontalCards;
