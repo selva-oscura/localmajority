0b22899fc0d66d906bb11c61b70213be42602355
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-01-19 11:04:59 -0800

    removed old API specs (for Nuxeo CMS' database)

--- src/api/cmsQueries.js
+++
@@ -1,9 +0,0 @@
-const cmsPath = 'https://cmsdev.localmajority.net/api';
-
-const cmsQueries = {
-  getAll: field => `${cmsPath}/${field}`,
-  getOneById: uid => `${cmsPath}/id/${uid}`,
-  getOneByPath: urlPath => `${cmsPath}/${urlPath}`,
-};
-
-export default cmsQueries;
