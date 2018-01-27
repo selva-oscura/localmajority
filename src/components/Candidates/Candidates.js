5b873b10b6ab9fc7f5a7fbd0417c61148004101d
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-01-27 00:34:17 -0800

    graphql-related updates: restored most commented-out code after updating variable names to reflect new graphql API; Filter still commented-out;

--- src/components/Candidates/Candidates.js
+++ src/components/Candidates/Candidates.js
@@ -8,76 +8,84 @@ class Candidates extends Component {
   constructor(props, context) {
     super(props, context);
     let candidatesStatesSelected = {},
-      candidatesSeatTypesSelected = {};
-    this.props.statesMasterList.forEach(stateName => {
-      candidatesStatesSelected[stateName] = true;
-    });
+      candidatesRegionTypesSelected = {};
+    if(this.props.statesMasterList){    
+      this.props.statesMasterList.forEach(stateName => {
+        candidatesStatesSelected[stateName] = true;
+      });
+    }
     this.state = {
       candidatesStatesSelected,
-      candidatesSeatTypesSelected,
+      candidatesRegionTypesSelected,
       candidatesTextSelected: [],
     };
+    console.log('this.props from constructor', this.props);
     this.updateFilter = this.updateFilter.bind(this);
   }
-  // updateFilter(filterCategory, selectedValues) {
-  //   let state = this.state;
-  //   state[filterCategory] = { ...selectedValues };
-  //   this.setState(state);
-  // }
+  updateFilter(filterCategory, selectedValues) {
+    let state = this.state;
+    state[filterCategory] = { ...selectedValues };
+    this.setState(state);
+  }
   componentDidMount() {
     document.title = 'Local Majority | Candidates';
   }
   render() {
-    console.log('this.props from Candidates page', this.props)
+    console.log('this.props from Candidates page', this.props);
     const { candidates, statesMasterList, regionTypesMasterList } = this.props;
     const {
       candidatesStatesSelected,
-      candidatesSeatTypesSelected,
+      candidatesRegionTypesSelected,
       candidatesTextSelected,
     } = this.state;
-    let candidatesMeetingFilters = candidates.filter(
-      candidate => candidatesStatesSelected[candidate.stateName]
+    let candidatesMeetingFilters = candidates && candidates.filter(
+      candidate => candidatesStatesSelected[candidate.state.title]
     );
-    return (<div><h2>Candidates page</h2></div>);
-    // return (
-    //   <div className="Candidates">
-    //     <Filters>
-    //       <SelectFilter
-    //         filterCategory="candidatesStatesSelected"
-    //         hintText="select state"
-    //         includeAll={true}
-    //         masterList={statesMasterList}
-    //         updateFilter={this.updateFilter}
-    //       />
-    //     </Filters>
-    //     {candidatesMeetingFilters.length ? (
-    //       <HorizontalCards>
-    //         {candidatesMeetingFilters.map((candidate, i) => {
-    //           let headshotUrl;
-    //           headshotUrl = candidate.headshotSmUrl
-    //             ? candidate.headshotSmUrl
-    //             : candidate.headshotLgUrl;
-    //           return (
-    //             <HorizontalCard
-    //               key={i}
-    //               id={candidate.id}
-    //               cardTitle={candidate.title}
-    //               cardSubtitle={candidate.seatName}
-    //               cardText={candidate.introLinkText}
-    //               category="candidates"
-    //               imgSrc={headshotUrl}
-    //               slug={candidate.slug}
-    //               imgShape="square"
-    //             />
-    //           );
-    //         })}
-    //       </HorizontalCards>
-    //     ) : (
-    //       <h2>No Candidates Meet Your Criteria.</h2>
-    //     )}
-    //   </div>
-    // );
+    return (
+      <div className="Candidates">
+        {candidatesMeetingFilters && candidatesMeetingFilters.length ? (
+          <HorizontalCards>
+            {candidatesMeetingFilters.map((candidate, i) => {
+              const headshotUrl = candidate.headshotId.url
+                ? candidate.headshotId.url
+                : null;
+              const seatTitle =
+                candidate &&
+                candidate.contestId &&
+                candidate.contestId.seatId &&
+                candidate.contestId.seatId.title
+                  ? candidate.contestId.seatId.title
+                  : null;
+              return (
+                <HorizontalCard
+                  key={i}
+                  id={candidate.id}
+                  cardTitle={candidate.title}
+                  cardSubtitle={seatTitle}
+                  cardText="none for right now"
+                  category="candidates"
+                  imgSrc={headshotUrl}
+                  slug={candidate.slug}
+                  imgShape="square"
+                />
+              );
+            })}
+          </HorizontalCards>
+        ) : (
+          <h2>No Candidates Meet Your Criteria.</h2>
+        )}
+      </div>
+    );
   }
 }
 
 export default Candidates;
+        // <Filters>
+        //   <SelectFilter
+        //     filterCategory="candidatesStatesSelected"
+        //     hintText="select state"
+        //     includeAll={true}
+        //     masterList={statesMasterList}
+        //     updateFilter={this.updateFilter}
+        //   />
+        // </Filters>
