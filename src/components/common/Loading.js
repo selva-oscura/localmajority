c82318000f6b56b6f7ec482f94fab965479cc0e4
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-01-28 00:00:46 -0800

    created Loading spinner

---
+++ src/components/common/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+
+const Loading = () => (
+  <div>
+    <h2>
+      Loading....
+      <CircularProgress />
+    </h2>
+  </div>
+);
+
+export default Loading;
