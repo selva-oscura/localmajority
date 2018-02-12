f40b7d33a2585eab0d2d2777fab4638595cc3f37
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-11 17:33:09 -0800

    auto-linting by prettier

--- src/components/Primers/Primer.js
+++ src/components/Primers/Primer.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Aux from '../common/Aux';
 
-const Primer = ({primer, i}) => (
+const Primer = ({ primer, i }) => (
   <Aux>
     <h3>{primer.title}</h3>
     {primer.content.map((section, s) => {
       const title =
-        section.heading && section.heading.title
-          ? section.heading.title
-          : null;
+        section.heading && section.heading.title ? section.heading.title : null;
       const body = section.body;
       const btype = body && body.type;
       if (!body && !title) {
@@ -18,10 +16,7 @@ const Primer = ({primer, i}) => (
         <Aux key={`primer-${i}-section-${s}`}>
           <div className={section.tag} key={section.tag}>
             {title && (
-              <div
-                className="section-head"
-                style={style.sectionHead}
-              >
+              <div className="section-head" style={style.sectionHead}>
                 {title}
               </div>
             )}
@@ -34,13 +29,9 @@ const Primer = ({primer, i}) => (
                 }}
               />
             )}
-            {btype === 'Image' && (
-              <img src={body.url} alt="alt text FIXME" />
-            )}
+            {btype === 'Image' && <img src={body.url} alt="alt text FIXME" />}
             {btype === 'PlainText' && (
-              <div className="section-body">
-                {body.text}
-              </div>
+              <div className="section-body">{body.text}</div>
             )}
           </div>
         </Aux>
