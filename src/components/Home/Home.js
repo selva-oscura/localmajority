ae072e5c9792dd88b5f22f9fb7af4d8d0f4c4e47
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-14 18:02:58 -0800

    conversion to new Home (remove VA map/text, added faces & org's description)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css';
 import Aside from '../common/Aside';
-import VADistrictMap from '../Maps/VADistrictMap';
 import Card from '../common/Cards/Card';
 
 const Home = props => {
@@ -12,23 +11,11 @@ const Home = props => {
   // let issuePrimers = articles.filter(a => a.type === 'IssuePrimer');
   return (
     <div className="Home">
-      <div className="map-and-text">
-        <div id="image">
-          <VADistrictMap />
-        </div>
-        <div className="caption">
-          <h2>November 7, 2017 .... It Starts with Virginia</h2>
-          <h3>
-            Electing a Congress <br />that Represents US
-          </h3>
-          <h3>
-            Polls Open<br />
-            22 Days 12 Hours 18 Minutes 5 Seconds
-          </h3>
-        </div>
-      </div>
       <div className="main-and-aside">
         <article className="Main">
+          <img src="../images/faces.png" alt="faces of candidates" />
+          <h2 className="text-center">Working Locally to Take Back State Legislatures</h2>
+          <p className="text-center">Local Majority provides research and other support for small state legislative campaigns who may not have many resources. Our goal is to build from the grassroots the local support that will help Democrats take back state legislatures across the country, beginning with Virginia in 2017, and continuing with Florida, Michigan and Minnesota in 2018.</p>
           <h3>Featured Candidates</h3>
           <div className="flex">
             {candidates && candidates.length ? (
