dca413b355a0ffc728fc3acef99a1a7be170c53c
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-14 18:03:25 -0800

    removed VA maps

--- src/components/Maps/VADistrictMap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import DistrictMap from './DistrictMap';
-import districtData from '../../data/va-house.json';
-
-const VADistrictMap = () => {
-  return <DistrictMap districtData={districtData} />;
-};
-
-export default VADistrictMap;
