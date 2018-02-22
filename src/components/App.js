420d70a7fe48a34d129f6e54ead4f1ffebb85d57
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-21 16:20:47 -0800

    restored state and saving to & retrieving as fallback from localStorage (updating of localStorage data moved to componentDidUpdate)

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,6 @@ import Candidates from './Candidates/Candidates';
 import District from './Districts/District';
 import Districts from './Districts/Districts';
 import Issues from './Issues/Issues';
-// import IssuePrimer from './Readings/IssuePrimer';
-// import ReadingHolder from './Readings/ReadingHolder';
 import AboutUs from './AboutUs/AboutUs';
 import Elements from './common/Elements';
 import FourZeroFour from './FourZeroFour';
@@ -22,58 +20,88 @@ import './App.css';
 injectTapEventPlugin();
 
 class App extends Component {
-  // constructor(props, context) {
-  //   super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-  // // check if there is local storage capability and stored data from last time
-  // let localMajorityData =
-  //   localStorage && localStorage.localMajorityData
-  //     ? localStorage.localMajorityData
-  //     : null;
-  // if (localMajorityData) {
-  //   // if there is stored data from last time, use that to bootstrap state (this will be fallback data in case of no internet access)
-  //   let state = JSON.parse(localMajorityData);
-  //   state.errors = [];
-  //   this.state = state;
-  // } else {
-  //   // default state if no localStorage or no stored data in localStorage
-  //   this.state = {
-  //     articles: [],
-  //     candidates: [],
-  //     contests: [],
-  //     issues: [],
-  //     parties: [],
-  //     seats: [],
-  //     statesMasterList: [],
-  //     regionTypesMasterList: [],
-  //     errors: [],
-  //   };
-  //   if (localStorage) {
-  //     // store data to localStorage if available
-  //     localStorage.localMajorityData = JSON.stringify(this.state);
-  //   }
-  // }
-  // console.log(
-  //   'this props from App....anything coming from graphql?',
-  //   '\n',
-  //   this.props
-  // );
-  // }
+    // check if there is local storage capability and stored data from last time
+    let localMajorityData =
+      localStorage && localStorage.localMajorityData
+        ? localStorage.localMajorityData
+        : null;
+    if (localMajorityData) {
+      // if there is stored data from last time, use that to bootstrap state (this will be fallback data in case of no internet access)
+      let state = JSON.parse(localMajorityData);
+      state.errors = [];
+      this.state = state;
+    } else {
+      // default state if no localStorage or no stored data in localStorage
+      this.state = {
+        candidates: [],
+        candidatesDetails: {},
+        seats: [],
+        seatsDetails: {},
+        parties: {},
+        states: [],
+        // articles: [],
+        // contests: [],
+        // issues: [],
+        statesMasterList: [],
+        regionTypesMasterList: [],
+        // errors: [],
+        timestamp: -Infinity,
+      };
+      if (localStorage) {
+        // store data to localStorage if available
+        localStorage.localMajorityData = JSON.stringify(this.state);
+      }
+    }
+    // this.setStateAndLocalStorage = this.setStateAndLocalStorage.bind(this);
+  }
 
-  // setStateAndLocalStorage(state) {
-  //   state = Object.assign({}, state);
-  //   this.setState(state);
-  //   if (localStorage) {
-  //     localStorage.localMajorityData = JSON.stringify(state);
-  //   }
-  // }
-  // componentDidMount(){
-  //   console.log(
-  //     'this props from App componentDidMount....anything coming from graphql?',
-  //     '\n',
-  //     this.props
-  //   );
-  // }
+  updateStateAndLocalStorage(state) {
+    state = {...state};
+    this.setState(state);
+    if (localStorage) {
+      localStorage.localMajorityData = JSON.stringify(state);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    let state = {...this.state};
+    Object.keys(this.props).forEach(query => {
+      if (prevProps[query].loading && !this.props[query].loading) {
+        if(query === "CandidatesBasics"){
+          state.candidates = this.props[query].allCandidates;
+        } else if (query === "SeatsBasics") {
+          state.seats = this.props[query].allSeats;
+        } else if (query === "States") {
+          state.states = this.props[query].allStates
+            .map(item => {
+              let { title, abbrev } = item;
+              return { title, abbrev }
+            }).sort((a,b) => {
+              if(a.title > b.title){
+                return 1;
+              } else {
+                return -1;
+              }
+            });
+            state.statesMasterList = state.states.map(state => (state.title))
+        } else if (query === "Parties") {
+          if (this.props.Parties && this.props.Parties.allParties) {
+            this.props[query].allParties.forEach(
+              party => (state.parties[`${party.id}`] = party)
+            );
+          }
+        }
+        state.timestamp = new Date().getTime();
+      }
+    });
+    if(prevProps !== this.props){
+      this.updateStateAndLocalStorage(state);
+    }
+  }
+
   renderPage() {
     // show Loading spinner if graphql queries are still being loaded
     Object.keys(this.props).forEach(query => {
@@ -83,30 +111,13 @@ class App extends Component {
     });
 
     // once queries are no longer being loaded, display content
-    const candidates = this.props.CandidatesBasics.allCandidates;
-    const seats = this.props.SeatsBasics.allSeats;
-    const statesMasterList =
-      this.props.States && this.props.States.allStates
-        ? this.props.States.allStates.map(state => state.title).sort()
-        : null;
-    const regionTypesMasterList = [
-      'Federal District',
-      'State',
-      'State District',
-    ];
-    const parties = {};
-    if (this.props.Parties && this.props.Parties.allParties) {
-      this.props.Parties.allParties.forEach(
-        party => (parties[`${party.id}`] = party)
-      );
-    }
+    const { candidates, seats, statesMasterList } = this.state;
 
-    // if (candidates && seats) {
-    //   console.log('candidates', candidates);
-    //   console.log('seats', seats);
-    //   console.log('parties', parties);
-    //   console.log('statesMasterList', statesMasterList);
-    // }
+    const regionTypesMasterList = [
+      {title: 'Federal District', abbrev: 'FD_US'},
+      {title: 'State', abbrev: 'State_US'},
+      {title: 'State District', abbrev: 'SD_US'},
+    ];
 
     return (
       <Switch>
@@ -125,7 +136,11 @@ class App extends Component {
               : { id: 'no-cached-data', headshotId: { url: null } };
             return (
               <ErrorBoundary>
-                <Candidate {...props} candidate={candidate} />
+                <Candidate
+                  {...props}
+                  candidate={candidate}
+                  updateStateAndLocalStorage={this.updateStateAndLocalStorage}
+                />
               </ErrorBoundary>
             );
           }}
@@ -179,7 +194,11 @@ class App extends Component {
               : { id: 'no-cached-data', mapSmUrl: { url: null } };
             return (
               <ErrorBoundary>
-                <District {...props} seat={seat} />
+                <District
+                  {...props}
+                  seat={seat}
+                  updateStateAndLocalStorage={this.updateStateAndLocalStorage}
+                />
               </ErrorBoundary>
             );
           }}
