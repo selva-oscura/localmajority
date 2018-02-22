5841e7e253e1b533f6696edd9a9f1e60fb28a15f
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-21 16:11:37 -0800

    added insetImg parameter and css to reduce image size and add padding around images; added text-area className and css to add padding around text for Card;

--- src/components/common/Cards/Card.js
+++ src/components/common/Cards/Card.js
@@ -22,8 +22,10 @@ class Card extends Component {
       category,
       slug,
       imgShape,
+      insetImg,
     } = this.props;
     let aspectRatio = imgShape || '';
+    let insetStyle = insetImg ? "insetImg" : '';
     return (
       <div
         className={`Card ${aspectRatio}`}
@@ -38,18 +40,20 @@ class Card extends Component {
                 alt={`placeholder for ${cardTitle}`}
                 src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23CCC%22%20d%3D%22M21%2019V5c0-1.1-.9-2-2-2H5c-1.1%200-2%20.9-2%202v14c0%201.1.9%202%202%202h14c1.1%200%202-.9%202-2zM8.5%2013.5l2.5%203.01L14.5%2012l4.5%206H5l3.5-4.5z%22%2F%3E%3C%2Fsvg%3E"
               />
-              <img className="actual-image" src={imgSrc} alt={cardTitle} />
+              <img className={`actual-image ${insetStyle}`} src={imgSrc} alt={cardTitle} />
             </div>
-            <h3>{cardTitle}</h3>
-            {cardSubtitle && <h4>{cardSubtitle}</h4>}
-            {cardText && <p>{cardText}</p>}
-            {cardTextHtml && (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: cardTextHtml,
-                }}
-              />
-            )}
+            <div className="text-area">
+              <h3>{cardTitle}</h3>
+              {cardSubtitle && <h4>{cardSubtitle}</h4>}
+              {cardText && <p>{cardText}</p>}
+              {cardTextHtml && (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: cardTextHtml,
+                  }}
+                />
+              )}
+            </div>
           </Link>
         </Paper>
       </div>
