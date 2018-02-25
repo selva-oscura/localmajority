95d0f56283b3825e87b7b49d4716dbad9c72e22d
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-24 19:33:11 -0800

    replaced standardizeDate(dateString) with prettifyDate(dateString), prettifyDateAndTime(dateString), prettifyTimestamp(timestamp); added Note re: timestamp versus dateString;

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,28 @@
-export function standardizeDate(dateString){
-	return `${dateString.slice(5, 7)}/${dateString.slice(8,10)}/${dateString.slice(0, 4)}`
+/*
+NOTE REGARDING INPUTS:
+	timestamp is the result of new Date().getTime()
+		e.g. 1517385346000
+	dateString is the string output of new Date()
+		e.g. "2018-01-31T07:55:46.000Z"
+*/
+export function prettifyDate(dateString){
+	let datetime = new Date(dateString);
+	return datetime.toLocaleDateString('en-US');
+}
+
+export function prettifyDateAndTime(dateString){
+	let datetime = new Date(dateString);
+	return `${datetime.toLocaleDateString('en-US')}, ${datetime.toLocaleTimeString('en-US')}`;
+}
+
+export function prettifyTimestamp(timestamp){
+	let datetime = new Date(timestamp);
+	console.log(datetime.toLocaleString('en-US'));
+	return `${datetime.toLocaleDateString('en-US')}, ${datetime.toLocaleTimeString('en-US')}`;
 };
+
+export default {
+	prettifyDate,
+	prettifyDateAndTime,
+	prettifyTimestamp,
+}
