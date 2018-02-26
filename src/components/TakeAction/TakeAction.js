4a70b5de144762b4ec99b7bee155da8c4efba07f
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-25 17:59:57 -0800

    created TakeAction component as stub

---
+++ src/components/TakeAction/TakeAction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './TakeAction.css';
+
+const TakeAction = () => {
+  return (
+    <article className="TakeAction">
+      <h1>Take Action</h1>
+    </article>
+  );
+};
+
+export default TakeAction;
