6fffdd414e97685b161706a6167aa23511ba0f94
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-25 20:32:05 -0800

    TakeAction: added different sections' headers & slogans (Voter Registration, Vote By Mail, Volnteer)

--- src/components/TakeAction/TakeAction.js
+++ src/components/TakeAction/TakeAction.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Filters from '../Filters/Filters';
 import ButtonlessFilters from '../Filters/ButtonlessFilters';
+import Aux from '../common/Aux';
 // import './TakeAction.css';
 
 class TakeAction extends Component {
@@ -14,14 +15,17 @@ class TakeAction extends Component {
 
   render(){
     const { statesMasterList } = this.props;
-
+    const regionText = this.state.stateSelected === "All" 
+      ? "America" 
+      : this.state.stateSelected;
     return (
       <article className="TakeAction">
         <div className="row">
           <div className="col-xs-12">
-            <h1>Take Action</h1>
             <section>
-              <h2>Choose Your State</h2>
+              <h2 className="text-right secondary-text-color">Your Vote is Your Voice</h2>
+              <h1>Take Action!</h1>
+              <h3>Choose Your State</h3>
               <Filters>              
                 <ButtonlessFilters
                   filterCategory="stateSelected"
@@ -29,8 +33,28 @@ class TakeAction extends Component {
                   updateFilter={this.updateFilter}
                 />
               </Filters>
+              <h2 className="tertiary-text-color">NOTE: content in red on this page is currently being used to specify content that will need to be changed in response to the individual state.  In the long-run we may want to use red for deadlines and other particularly emphasized issues....</h2>
               <p>Filter: {this.state.stateSelected}</p>
             </section>
+            <hr />
+            <section>
+              <h2 className="text-right secondary-text-color">Voting is not a Spectator Sport</h2>
+              <h2>Voter Registration</h2>
+            </section>
+            <hr />
+            <section>
+              <h2 className="text-right secondary-text-color">Your Vote Counts More than Ever!<br />
+                &amp; it is the Only Way to turn {regionText} Blue Again</h2>
+              <h2>Vote By Mail</h2>
+            </section>
+            <hr />
+            <section>
+              <h2 className="text-right secondary-text-color">TIME TO GET OUT THE VOTE!!</h2>
+              <h2>Volunteer</h2>
+            </section>
+            <hr />
+            <h2 className="text-right secondary-text-color">DEMOCRACY’S ONLY HOPE!</h2>
+
           </div>
         </div>
       </article>
