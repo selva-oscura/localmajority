91168f4624dc2ed5d70485fd61f35780ab2ce8c8
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-02-25 19:44:08 -0800

    TakeAction: added filter for selecting state (made component stateful, added stateSelected, added ButtonlessFilters)

--- src/components/TakeAction/TakeAction.js
+++ src/components/TakeAction/TakeAction.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Filters from '../Filters/Filters';
+import ButtonlessFilters from '../Filters/ButtonlessFilters';
 // import './TakeAction.css';
 
-const TakeAction = () => {
-  return (
-    <article className="TakeAction">
-      <h1>Take Action</h1>
-    </article>
-  );
-};
+class TakeAction extends Component {
+  state = {
+    stateSelected: "All",
+  }
+
+  updateFilter = (filterCategory, selectedValues) => {
+    this.setState({stateSelected: selectedValues});
+  };
+
+  render(){  
+    const { statesMasterList } = this.props;
+
+    return (
+      <article className="TakeAction">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Take Action</h1>
+            <section>
+              <h2>Choose Your State</h2>
+              <Filters>              
+                <ButtonlessFilters
+                  filterCategory="stateSelected" // <- this is the name of the variable used as the filter in the parent component
+                  masterList={statesMasterList}            // <- sorted array with all possible values (excepting all/none)
+                  updateFilter={this.updateFilter}         // <- parent component's function for updating updating the filter that will be used to display parent component's candidates, articles, districts, etc.
+                />
+              </Filters>
+              <p>Filter: {this.state.stateSelected}</p>
+            </section>
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
 
 export default TakeAction;
