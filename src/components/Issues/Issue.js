f4270d1f1e8f192c0e076a4a65819404b650973f
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-01 21:52:14 -0800

    Issue: replaced previous article content display with list of the current issue's subissues and their articles

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -1,47 +1,32 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Aux from '../common/Aux';
 
-// class Issue extends Component {
-//   componentDidMount() {
-//     const url = 'http://selva-oscura.net/projects/localMajority/articles/';
-//     const param = this.props.match.params.id;
-//     axios
-//       .get(url + param + '.html')
-//       .then(res => {
-//         if (res.status === 200) {
-//           document.getElementById('article-space').innerHTML = res.data;
-//         } else {
-//           console.log('error fetching', param, 'status', res.status, '\n', res);
-//           document.getElementById('article-space').innerHTML =
-//             "<p>We're sorry, but there was an error fetching your article.</p>";
-//         }
-//       })
-//       .catch(err => {
-//         console.log('error fetching', param, err);
-//         document.getElementById('article-space').innerHTML =
-//           "<p>We're sorry, but there was an error fetching your article.</p>";
-//       });
-//   }
-//   render() {
-//     console.log('this.props', this.props);
-//     const issue = this.props.issue;
-//     // console.log('props from Issue', this.props);
-//     // console.log('issue from Issue', issue);
-//     return (
-//       <div className="Issue">
-//         <h2>{issue.title}</h2>
-//         <h3>by {issue.author}</h3>
-//         <h6>
-//           {issue.time.slice(0, issue.time.length - 15)} --{' '}
-//           {Math.floor(issue.contentLength / 400)} minute read
-//         </h6>
-//         <p>
-//           <b>Summary:</b> {issue.summary}
-//         </p>
-//         <div id="article-space" />
-//         <div id="candidate-space" />
-//       </div>
-//     );
-//   }
-// }
+class Issue extends Component {
+  render() {
+    const { issue } = this.props;
+    const { subIssues } = issue;
+    return (
+      <div className="Issue">
+        <h2>{issue.title}</h2>
+        {subIssues.map((subIssue, i) => (
+          <Aux key={i}>
+            <h4>{subIssue.title}</h4>
+            {subIssue.articles.map((article, j) => (
+              <Aux key={j}>
+                <Link to={`/articles/${article.slug}`}>
+                  <p className="row">
+                    <span className="col-sm-4">{article.articleType}</span>
+                    <span className="col-sm-8">{article.title}</span>
+                  </p>
+                </Link>
+              </Aux>
+            ))}
+          </Aux>
+        ))}
+      </div>
+    );
+  }
+}
 
-// export default Issue;
+export default Issue;
