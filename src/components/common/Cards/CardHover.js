b2332a735afd1c0d7eed24b2ee0f803a7f003a9b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-04 16:49:16 -0800

    CardHover: created hover depth effect as standalone component

---
+++ src/components/common/Cards/CardHover.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import PropTypes from 'prop-types';
+import './CardHover.css';
+
+class CardHover extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { depth: 2 };
+  }
+  onMouseOver = () => this.setState({ depth: 3 });
+  onMouseOut = () => this.setState({ depth: 2 });
+  render() {
+    let {
+      imgShape,
+    } = this.props;
+    let aspectRatio = imgShape || '';
+    return (
+      <div
+        className={`CardHover ${aspectRatio}`}
+        onMouseOver={this.onMouseOver}
+        onMouseOut={this.onMouseOut}
+      >
+        <Paper zDepth={this.state.depth} style={{height: "100%"}}>
+          {this.props.children}
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default CardHover;
