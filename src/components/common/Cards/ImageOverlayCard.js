f35aaf07955124e41990d3c1d0ef9ee1b5b4a078
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-04 18:56:10 -0800

    ImageOverlayCard: made component stateless; added/used props (cardTitle, imgSrc); styled component;

--- src/components/common/Cards/ImageOverlayCard.js
+++ src/components/common/Cards/ImageOverlayCard.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import {Card, CardMedia, CardTitle} from 'material-ui/Card';
+import React from 'react';
+import { Card, CardMedia, CardTitle } from 'material-ui/Card';
 
-class ImageOverlayCard extends Component {
-	render(){
-		return (
-		  <Card>
-		    <CardMedia
-		      overlay={<CardTitle title="Overlay title" />}
-		      overlayContentStyle={{height: "100%"}}
-		    >
-		      <img src="../images/District_1_map.png" alt="" />
-		    </CardMedia>
-		  </Card>
-		)
-	}
-}
+const ImageOverlayCard = ({ cardTitle="title missing", imgSrc }) => (
+  <Card>
+    <CardMedia
+      overlay={<CardTitle title={cardTitle} titleStyle={{marginLeft: "12px"}} />}
+      overlayContentStyle={{height: "100%", background: "rgba(0, 0, 0, 0.54)", background: "-webkit-linear-gradient(-45deg, rgba(0, 0, 0, .85) 0%, rgba(0, 0, 0, 0.4) 100%)"}}
+      style={{height: "150px", overflow: "hidden",}}
+    >
+			{imgSrc && <img src={imgSrc} alt={cardTitle} />}
+    </CardMedia>
+  </Card>
+);
 
 export default ImageOverlayCard;
