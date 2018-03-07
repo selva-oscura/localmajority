08e3d4ee0dc9c558ef494b962be52eddb278d81a
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-06 20:06:59 -0800

    FootCard: added placeholder background image and forced min height of image space equal to width

--- src/components/common/Cards/FooterCard.js
+++ src/components/common/Cards/FooterCard.js
@@ -4,29 +4,38 @@ import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 const FooterCard = ({ cardTitle="title missing", cardSubtitle='', cardText='', imgSrc, insetImg='',}) => {
 	const inset = insetImg 
 	? { 
-			margin: "1.5vw 1.5vw 0 1.5vw", 
+			margin: "calc(-100% + 1.5vw) 1.5vw 0 1.5vw",
 			maxWidth: "calc(100% - 3vw)",
 			minWidth: "calc(100% - 3vw)",
 			width: "calc(100% - 3vw)",
 		}
-	: null
+	: {
+			marginTop: "-100%"
+		};
 	return (
-	  <Card
-	  	className="FooterCard"
-	  	containerStyle={{paddingBottom: "0"}}
-	  >
-	    <CardMedia>
+		<Card
+			className="FooterCard"
+			containerStyle={{paddingBottom: "0", width: "100%", paddingTop:"100%", background: "url('images/placeholderImage.svg') no-repeat", backgroundSize: "100%", backgroundPosition: "center 0"}}
+		>
+			<CardMedia>
 				<img src={imgSrc} alt={cardTitle} style={inset} />
-	    </CardMedia>
-	    <CardTitle 
-	    	title={cardTitle}
-	    	titleStyle={{color: "rgba(0, 110, 237, 0.75)", fontSize: "1.17em"}}
-	    	subtitle={cardSubtitle}
-	    	subtitleStyle={{color: "rgba(0, 110, 237, 0.75)"}}
-	    	style={{padding: "12px"}}
-	    />
-	    <CardText>{cardText}</CardText>
-	  </Card>
+			</CardMedia>
+			{ cardSubtitle
+				? <CardTitle
+					title={cardTitle}
+					titleStyle={{color: "rgba(0, 110, 237, 0.75)", fontSize: "1.17em"}}
+					subtitle={cardSubtitle}
+					subtitleStyle={{color: "rgba(0, 110, 237, 0.75)"}}
+					style={{padding: "0 12px 12px 12px", marginTop: "-12px"}}
+				/>
+				: <CardTitle
+					title={cardTitle}
+					titleStyle={{color: "rgba(0, 110, 237, 0.75)", fontSize: "1.17em"}}
+					style={{padding: "0 12px 12px 12px", marginTop: "-12px"}}
+				/>
+			}
+			{ cardText && <CardText>{cardText}</CardText> }
+		</Card>
 	)
 };
 
