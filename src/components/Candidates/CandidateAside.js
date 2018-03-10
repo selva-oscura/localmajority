ed4b7eefcd45ccc92d523c764b820d678e91edd7
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-10 02:13:04 -0800

    CandidateAside: made display of 'Contact the Candidate' contingent on some contact info existing, made CandidateAside content full-width for large layouts

--- src/components/Candidates/CandidateAside.js
+++ src/components/Candidates/CandidateAside.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../common/Aside.css';
+import Aux from '../common/Aux';
 import TwitterTimeline from '../common/TwitterTimeline';
 import { SocialIcon } from 'react-social-icons';
 import CandidateDonateButton from './CandidateDonateButton';
@@ -7,39 +8,45 @@ import CandidateWebsiteButton from './CandidateWebsiteButton';
 
 const CandidateAside = props => {
   const candidate = props.candidate;
+  const anyContactInfo = candidate.facebook || candidate.twitter || candidate.campaignEmail || candidate.homepageUrl || candidate.donateUrl
+    ? true
+    : false;
   return (
     <aside id="Aside">
       <div className="row">
-        <div className="col-12 col-sm-6">
-          {candidate.twitter && (
+        <div className="col-12 col-sm-6 col-lg-12">
+          { candidate.twitter && (
             <TwitterTimeline twitterHandle={candidate.twitter} />
           )}
         </div>
-        <div className="col-12 col-sm-6 text-right">
-          <h3>Contact the Candidate</h3>
-          <div className="social-icons-space">
-            {candidate.facebook && (
-              <SocialIcon
-                url={`https://www.facebook.com/${candidate.facebook}`}
-              />
-            )}
-            {candidate.twitter && (
-              <SocialIcon url={`https://twitter.com/${candidate.twitter}`} />
-            )}
-            {candidate.campaignEmail && (
-              <SocialIcon
-                url={`mailto:${candidate.campaignEmail}`}
-                network="email"
-                color="#E4002D"
-              />
-            )}
-          </div>
-          {candidate.homepageUrl && (
-            <CandidateWebsiteButton candidate={props.candidate} />
-          )}
-          {candidate.donateUrl && (
-            <CandidateDonateButton candidate={props.candidate} />
-          )}
+        <div className="col-12 col-sm-6 hidden-lg-up text-right">
+          { anyContactInfo && <Aux>
+              <h3>Contact the Candidate</h3>
+              <div className="social-icons-space">
+                {candidate.facebook && (
+                  <SocialIcon
+                    url={`https://www.facebook.com/${candidate.facebook}`}
+                  />
+                )}
+                {candidate.twitter && (
+                  <SocialIcon url={`https://twitter.com/${candidate.twitter}`} />
+                )}
+                {candidate.campaignEmail && (
+                  <SocialIcon
+                    url={`mailto:${candidate.campaignEmail}`}
+                    network="email"
+                    color="#E4002D"
+                  />
+                )}
+              </div>
+              {candidate.homepageUrl && (
+                <CandidateWebsiteButton candidate={props.candidate} />
+              )}
+              {candidate.donateUrl && (
+                <CandidateDonateButton candidate={props.candidate} />
+              )}
+            </Aux>
+          }
         </div>
       </div>
     </aside>
