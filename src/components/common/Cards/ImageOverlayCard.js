b844eb637268d136b4b3a2dcc6229521065ff57b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-10 20:53:09 -0800

    ImageOverlayCard: added spacing to overlayContentStyle & removed duplicate background declaration

--- src/components/common/Cards/ImageOverlayCard.js
+++ src/components/common/Cards/ImageOverlayCard.js
@@ -5,7 +5,10 @@ const ImageOverlayCard = ({ cardTitle="title missing", imgSrc }) => (
   <Card>
     <CardMedia
       overlay={<CardTitle title={cardTitle} titleStyle={{marginLeft: "12px"}} />}
-      overlayContentStyle={{height: "100%", background: "rgba(0, 0, 0, 0.54)", background: "-webkit-linear-gradient(-45deg, rgba(0, 0, 0, .85) 0%, rgba(0, 0, 0, 0.4) 100%)"}}
+      overlayContentStyle={{
+        height: "100%",
+        background: "-webkit-linear-gradient(-45deg, rgba(0, 0, 0, .85) 0%, rgba(0, 0, 0, 0.4) 100%)"
+      }}
     >
 			{imgSrc && <img src={imgSrc} alt={cardTitle} />}
     </CardMedia>
