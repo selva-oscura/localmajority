99cad1bbf2364af61d33cc2880464c46d251f45a
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-12 00:52:32 -0700

    States: converted 'Our States' from Home to standalone component used on Home and /Districts

---
+++ src/components/States/States.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
+import CardHover from '../common/Cards/CardHover';
+import FooterCard from '../common/Cards/FooterCard';
+
+const States = ({currentStateRaces}) => (
+  <section className="col-12">
+    <h2 className="text-center">Our States</h2>
+    <div className="flex">
+      {currentStateRaces.map((state, i) => (
+        <GridXSmallIsOneSmallIsThree key={i}>
+          <CardHover>
+            <Link to={`./districts/${state.title}`}>
+              <FooterCard
+                cardTitle={state.title}
+                imgSrc={`../images/${state.title}.jpg`}
+                insetImg="insetImg"
+              />
+            </Link>
+          </CardHover>
+        </GridXSmallIsOneSmallIsThree>
+      ))}
+    </div>
+  </section>
+)
+
+export default States;
