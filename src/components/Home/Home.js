438a1efea0c6713fde2ba6559f866f89318fc861
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-15 23:14:07 -0700

    Home: replaced random faces splash image with headshots/data of current and past candidates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,27 +2,76 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import States from '../States/States';
 import HomeAside from './HomeAside';
-import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import GridXSmallIsOneSmIsTwoMedIsThreeLargeIsFour from '../common/Grids/GridXSmallIsOneSmIsTwoMedIsThreeLargeIsFour';
 import CardHover from '../common/Cards/CardHover';
 import FooterCard from '../common/Cards/FooterCard';
+import candidatesVA2017 from '../../data/candidatesVA2017';
 import { prettifyDate } from '../../utils/functions';
 import './Home.css';
 
 const Home = props => {
   document.title = 'Local Majority';
 
-  const { articles, candidates, currentStateRaces } = props;
+  const { articles, candidates, currentStateRaces, pastCandidates } = props;
+
+  const facesCandidates = pastCandidates.map(candidate => {
+      return {
+        title: candidate['title'],
+        district: `${candidate['state']['title']} ${candidate['contestId']['seatId']['title'].slice(3)}`,
+        won: false,
+        electionPast: true,
+        electionYear: Number(candidate['contestId']['electionDate'].slice(0,4)),
+        headshot: candidate['headshotId']['url'],
+      }
+    })
+  .concat(candidatesVA2017)
+  .concat(candidates.slice(0, 18-(candidatesVA2017.length + pastCandidates.length))
+    .map(candidate => {
+      return {
+        title: candidate['title'],
+        district: `${candidate['state']['title']} ${candidate['contestId']['seatId']['title'].slice(3)}`,
+        won: false,
+        electionPast: false,
+        electionYear: Number(candidate['contestId']['electionDate'].slice(0,4)),
+        slug: candidate['title'].split(' ').join('-').toLowerCase(),
+        headshot: candidate['headshotId']['url'],
+      }
+    })
+  );
+
+  const presentationForCandidatesFaces = [];
+  const numberOfRowsForFaces = 2;
+  for(let i = 0; i<numberOfRowsForFaces; i++){
+    presentationForCandidatesFaces.push(facesCandidates.slice(i*(facesCandidates.length/numberOfRowsForFaces), (i+1)*(facesCandidates.length/numberOfRowsForFaces)))
+  };
 
   return (
     <div className="Home">
       <div className="row">
         <section className="col-12">
-          <img
-            className="splash-image"
-            src="../images/faces.png"
-            alt="faces of candidates"
-          />
+          <div className="Faces" style={{padding: "24px 0"}}>
+            { presentationForCandidatesFaces.map((rowContents, i) =>
+              <div className="row no-gutters" key={i}>
+                { rowContents.map((candidate, j) => {
+                  const imageAlternatives = ['hala-ayala', 'david-reid', 'chris-hurst'];
+                  const imgSrc = candidate.headshot
+                    ? candidate.headshot
+                    : `../images/candidates_of_yore/${imageAlternatives[j%3]}.jpg`;
+                  return (
+                    <div className="col" key={j} style={{ padding: '1px', background: "url('images/placeholderImage.svg') no-repeat", backgroundSize: "100%", backgroundPosition: "center 0", overflow: 'hidden'}}>
+                      <img
+                        className="full-width"
+                        src={imgSrc}
+                        alt={`${candidate.title}, Local Majority backed ${candidate.electionYear} candidate for ${candidate.district}`}
+                        title={`${candidate.title}, Local Majority backed ${candidate.electionYear} candidate for ${candidate.district}`}
+                        style={{ textIndent: "100%", whiteSpace: "nowrap", overflow: "hidden",}}
+                      />
+                    </div>
+                  )}
+                )}
+              </div>
+            )}
+          </div>
           <h2 className="text-center">
             Working Locally to Take Back State Legislatures
           </h2>
