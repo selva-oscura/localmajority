67ccb2b6632213a286eeb4c20dd194ae04a2c09c
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-03-22 23:02:02 -0700

    Home: changed source data for candidates faces, added 2nd copy of candidates faces (one colour background, one grey background), put slogan above faces, changed bootstrap class from flext to row

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,24 +14,25 @@ const Home = props => {
 
   const { articles, candidates, currentStateRaces, pastCandidates } = props;
 
-  const facesCandidates = pastCandidates
-    .map(candidate => {
-      return {
-        title: candidate['title'],
-        district: `${candidate['state']['title']} ${candidate['contestId'][
-          'seatId'
-        ]['title'].slice(3)}`,
-        won: true,
-        electionYear: Number(
-          candidate['contestId']['electionDate'].slice(0, 4)
-        ),
-        headshot: candidate['headshotId']['url'],
-      };
-    })
+  const facesCandidates = []
+  // pastCandidates
+  //   .map(candidate => {
+  //     return {
+  //       title: candidate['title'],
+  //       district: `${candidate['state']['title']} ${candidate['contestId'][
+  //         'seatId'
+  //       ]['title'].slice(3)}`,
+  //       won: true,
+  //       electionYear: Number(
+  //         candidate['contestId']['electionDate'].slice(0, 4)
+  //       ),
+  //       headshot: candidate['headshotId']['url'],
+  //     };
+  //   })
     .concat(candidatesVA2017)
     .concat(
       candidates
-        .slice(0, 18 - (candidatesVA2017.length + pastCandidates.length))
+        .slice(0, 18 - (candidatesVA2017.length))
         .map(candidate => {
           return {
             title: candidate['title'],
@@ -66,15 +67,66 @@ const Home = props => {
     <div className="Home">
       <div className="row">
         <section className="col-12">
-          <div className="Faces" style={{ padding: '24px 0' }}>
+          <h2 className="text-center">
+            Working Locally to Take Back State Legislatures
+          </h2>
+          <p className="text-center">
+            Local Majority provides research and other support for small state
+            legislative campaigns who may not have many resources. Our goal is
+            to build from the grassroots the local support that will help
+            Democrats take back state legislatures across the country, beginning
+            with Virginia in 2017, and continuing with Florida, Michigan and
+            Minnesota in 2018.
+          </p>
+
+          <div className="FacesColourBackground" style={{ padding: '24px 0' }}>
             {presentationForCandidatesFaces.map((rowContents, i) => (
               <div className="row no-gutters" key={i}>
                 {rowContents.map((candidate, j) => {
                   const imgSrc = candidate.headshot
                     ? candidate.headshot
-                    : `../images/candidates_of_yore/${
-                        candidate.slug
-                      }.jpg`;
+                    : `../images/candidates_of_yore/color/${candidate.slug}.png`;
+                  return (
+                    <div
+                      className="col"
+                      key={j}
+                      style={{
+                        padding: '1px',
+                        background:
+                          "url('images/placeholderImage.svg') no-repeat",
+                        backgroundSize: '100%',
+                        backgroundPosition: 'center 0',
+                        overflow: 'hidden',
+                      }}
+                    >
+                      <img
+                        className="full-width"
+                        src={imgSrc}
+                        alt={`${candidate.title}, Local Majority backed ${
+                          candidate.electionYear
+                        } candidate for ${candidate.district}`}
+                        title={`${candidate.title}, Local Majority backed ${
+                          candidate.electionYear
+                        } candidate for ${candidate.district}`}
+                        style={{
+                          textIndent: '100%',
+                          whiteSpace: 'nowrap',
+                          overflow: 'hidden',
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            ))}
+          </div>  
+          <div className="FacesGreyBackground" style={{ padding: '24px 0' }}>
+            {presentationForCandidatesFaces.map((rowContents, i) => (
+              <div className="row no-gutters" key={i}>
+                {rowContents.map((candidate, j) => {
+                  const imgSrc = candidate.headshot
+                    ? candidate.headshot
+                    : `../images/candidates_of_yore/${candidate.slug}.jpg`;
                   return (
                     <div
                       className="col"
@@ -109,24 +161,13 @@ const Home = props => {
               </div>
             ))}
           </div>
-          <h2 className="text-center">
-            Working Locally to Take Back State Legislatures
-          </h2>
-          <p className="text-center">
-            Local Majority provides research and other support for small state
-            legislative campaigns who may not have many resources. Our goal is
-            to build from the grassroots the local support that will help
-            Democrats take back state legislatures across the country, beginning
-            with Virginia in 2017, and continuing with Florida, Michigan and
-            Minnesota in 2018.
-          </p>
         </section>
 
         <States currentStateRaces={currentStateRaces} />
 
         <section className="col-12">
           <h2 className="text-center">Featured Candidates</h2>
-          <div className="flex">
+          <div className="row">
             {candidates && candidates.length ? (
               candidates.map((candidate, i) => {
                 const headshotUrl =
