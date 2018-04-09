fe4111ee4d6adc6cc812f54be2546617a641f7cc
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-08 20:31:57 -0700

    NoSuchArticle: autolinting by prettier

--- src/components/Articles/NoSuchArticle.js
+++ src/components/Articles/NoSuchArticle.js
@@ -5,7 +5,7 @@ const NoSuchArticle = ({ candidateId }) => {
   return (
     <div className="NoSuchArticle">
       <h2>Error fetching Article</h2>
-      <Link to="/issues">Return to List of Issues</Link>
+      <Link to="/research">Return to List of Issues</Link>
     </div>
   );
 };
