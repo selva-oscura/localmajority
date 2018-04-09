e5cc8576a27135a5d9fa2de138cf4a418c41aad5
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-08 20:53:47 -0700

    Issue: created fallback to NoSuchIssue if invalid issue url

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import NoSuchIssue from './NoSuchIssue';
 import Aux from '../common/Aux';
 
 class Issue extends Component {
   componentDidMount() {
-    console.log('props from Issue', this.props)
     this.props.issue && this.props.issue.title
       ? (document.title = `Local Majority | Research | ${
           this.props.issue.title
@@ -14,9 +14,12 @@ class Issue extends Component {
 
   render() {
     const { issue } = this.props;
-    const { subIssues } = issue
-      ? issue
-      : null;
+
+    if(!issue){
+      return <NoSuchIssue issueParam={this.props.match.params.slug}/>
+    }
+
+    const { subIssues } = issue;
 
     return (
       <div className="Issue">
