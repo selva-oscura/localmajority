1b479f991bb495c56c699f3a344a54a698aaae4e
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-09 22:19:36 -0700

    Footer: added legal disclaimer and navlinks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import navLinks from '../../navLinks.js';
+import './Footer.css';
 
-const Footer = () => <footer className="Footer full-width"><p>Footer</p></footer>
+const Footer = () => (
+	<footer className="Footer full-width">
+		<div className="container">
+			<p className="text-center">PAID FOR BY LOCAL MAJORITY AND NOT AUTHORIZED BY ANY CANDIDATES OR CANDIDATE’S COMMITTEE.<br />
+			&copy;2018 LOCAL MAJORITY. ALL RIGHTS RESERVED. LOCALMAJORITY.ORG LOCALMAJORITY.ORG</p>
+			<div className="hidden-sm-down">
+	      <nav>
+	        {navLinks.map((link, i) => (
+	          <NavLink
+	            key={i}
+	            to={link.path}
+	            exact={link.exact}
+	            activeClassName="selected"
+	          >
+	            {link.name}
+	          </NavLink>
+	        ))}
+	      </nav>
+			</div>
+		</div>
+	</footer>
+);
 
 export default Footer;
