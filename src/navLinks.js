e5fe6aa69bcd8afe11cab5548336b9567cbedecb
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-09 22:13:01 -0700

    navLinks: created navLinks as exported constant

---
+++ src/navLinks.js
@@ -0,0 +1,28 @@
+export default [
+  { path: '/', name: 'Home', shortName: 'Home', exact: true },
+  { path: '/research', name: 'Issues', shortName: 'Issues', exact: false },
+  {
+    path: '/districts',
+    name: 'States',
+    shortName: 'States',
+    exact: false,
+  },
+  {
+    path: '/candidates',
+    name: 'Candidates',
+    shortName: 'Candidates',
+    exact: false,
+  },
+  {
+    path: '/take-action',
+    name: 'Take Action!',
+    shortName: 'Act!',
+    exact: true,
+  },
+  {
+    path: '/about-us',
+    name: 'About Us',
+    shortName: 'About Us',
+    exact: true,
+  },
+];
