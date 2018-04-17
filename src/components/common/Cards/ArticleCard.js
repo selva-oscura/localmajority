6cfa2e342554c46f71c541a9245d4e12b7909ac4
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-16 23:29:18 -0700

    ArticleCard: added individual links for tags, articleType, flex styling

--- src/components/common/Cards/ArticleCard.js
+++ src/components/common/Cards/ArticleCard.js
@@ -3,23 +3,34 @@ import { Link } from 'react-router-dom';
 import ImageWithBackgroundPlaceholderImage from '../ImageWithBackgroundPlaceholderImage';
 import CardHover from './CardHover';
 import { prettifyDate } from '../../../utils/functions';
+import './ArticleCard.css';
 
 const ArticleCard = ({slug, imageSrc, imageAlt="", title, author, updatedAt, tagRoute="", tags=[]}) => (
   <CardHover>
-    <Link
-      to={`articles/${slug}`}
-    >
-      <ImageWithBackgroundPlaceholderImage
-        imageSrc={imageSrc}
-        imageAlt=""
-        aspectRatioInPercent="75"
-      />
-      <h2>
-        <span className="title">{title}</span>
-        <span className="author">{` by ${author}`}</span></h2>
-      <h4 className="text">{prettifyDate(updatedAt)}</h4>
-      <h4 className="tags">{tags.join(", ")}</h4>
-    </Link>
+  	<div className="ArticleCard">
+	    <Link
+	      to={`articles/${slug}`}
+	    >
+	      <ImageWithBackgroundPlaceholderImage
+	        imageSrc={imageSrc}
+	        imageAlt=""
+	        aspectRatioInPercent="75"
+	      />
+	      <div className="contents">
+		      <h2 className="title">{title}</h2>
+		      <p className="details flex-container flex-wrap flex-space-between">
+		      	<span>{`${author}`}</span>
+		      	<span>updated {prettifyDate(updatedAt)}</span>
+		      </p>
+	      </div>
+	    </Link>
+      <div className="tags flex-container flex-wrap flex-start">
+      	{tags.map(tag => (
+      		<Link key={tag} to={`/${tagRoute}/${tag}`} className="tag">{tag}
+      		</Link>
+      	))}
+      </div>
+	   </div>
   </CardHover>
 );
 
