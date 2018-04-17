f1dcacb5b7f3ddf9a98b4e06095091ea2fc09348
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-16 18:26:13 -0700

    Issue: added ArticleLink to display of issue's articles (comparative display of ArticleLink older display)

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NoSuchIssue from './NoSuchIssue';
+import ArticleLink from '../common/ArticleLink';
 import Aux from '../common/Aux';
 
 class Issue extends Component {
@@ -13,34 +14,57 @@ class Issue extends Component {
   }
 
   render() {
-    const { issue } = this.props;
+    const { issue, articles } = this.props;
+    const subIssues = issue
+      ? issue.subIssues
+      : [];
 
-    if (!issue) {
-      return <NoSuchIssue issueParam={this.props.match.params.slug} />;
-    }
-
-    const { subIssues } = issue;
-
-    return (
+    return issue
+      ? (
       <div className="Issue">
-        <h2>{issue.title}</h2>
-        {subIssues.map((subIssue, i) => (
-          <Aux key={i}>
+        <h2>{issue.title} -- HEY!  COMPARE THIS WITH A NON-ISSUE TAG</h2>
+        {subIssues.map(subIssue => (
+          <Aux key={subIssue.title}>
             <h4>{subIssue.title}</h4>
-            {subIssue.articles.map((article, j) => (
-              <Aux key={j}>
-                <Link to={`/articles/${article.slug}`}>
-                  <p className="row">
-                    <span className="col-sm-4">{article.articleType}</span>
-                    <span className="col-sm-8">{article.title}</span>
-                  </p>
-                </Link>
-              </Aux>
-            ))}
+            {articles.map(article => {
+              let articleThumbnail = article.thumbnail ? article.thumbnail : "../images/economy.jpg";
+              return (
+                article.tags.includes(subIssue.slug)
+                ? (
+                  <ArticleLink
+                    key={`${subIssue.title}-${article.slug}`}
+                    article={article}
+                    slug={article.slug}
+                    imageSrc={articleThumbnail}
+                    title={article.title}
+                    articleType={article.articleType}
+                    author={article.author}
+                    updatedAt={article.updatedAt}
+                    tagRoute="research"
+                    tags={article.tags}
+                  />
+                )
+                : null
+              )}
+            )}
           </Aux>
         ))}
       </div>
-    );
+    ) : (
+      <div className="Issue">
+        <h2>{this.props.match.params.slug} -- HEY!  COMPARE THIS WITH AN ISSUE TAG</h2>
+        {articles.map((article, i) => (
+          <Aux key={i}>
+            <Link to={`/articles/${article.slug}`}>
+              <p className="row">
+                <span className="col-sm-4">{article.articleType}</span>
+                <span className="col-sm-8">{article.title} -- {article.tags.join(', ')}}</span>
+              </p>
+            </Link>
+          </Aux>
+        ))}
+      </div>
+    )
   }
 }
 
