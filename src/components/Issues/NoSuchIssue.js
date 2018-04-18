7aebc9f936d977bb4149629276518059cbe48933
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-17 18:05:47 -0700

    NoSuchIssue: changed 'not found' text

--- src/components/Issues/NoSuchIssue.js
+++ src/components/Issues/NoSuchIssue.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NoSuchIssue = ({ issueParam }) => {
   return (
     <div className="NoSuchIssue">
-      <h2>We're sorry. {issueParam} not found</h2>
+      <h2>We're sorry. No articles match the term '{issueParam}'.</h2>
       <Link to="/research">Return to List of Issues</Link>
     </div>
   );
