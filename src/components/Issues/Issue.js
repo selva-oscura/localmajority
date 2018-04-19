a9641a0339d17476727c5d890014e19fa08f1cf6
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-19 16:35:02 -0700

    Issue: relatedArticles filtered by tag that isn't an Issue now displayed with ArticleLink component, changed fallback image

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -19,14 +19,14 @@ class Issue extends Component {
 
     return issue ? (
       <div className="Issue">
-        <h2>{issue.title} -- HEY! COMPARE THIS WITH A NON-ISSUE TAG</h2>
+        <h2>{issue.title}</h2>
         {subIssues.map(subIssue => (
           <Aux key={subIssue.title}>
             <h4>{subIssue.title}</h4>
             {articles.map(article => {
               let articleThumbnail = article.thumbnail
                 ? article.thumbnail
-                : '../images/economy.jpg';
+                : 'https://placekitten.com/400/300';
               return article.tags.includes(subIssue.slug) ? (
                 <ArticleLink
                   key={`${subIssue.title}-${article.slug}`}
@@ -47,21 +47,26 @@ class Issue extends Component {
       </div>
     ) : (
       <div className="Issue">
-        <h2>
-          {this.props.match.params.slug} -- HEY! COMPARE THIS WITH AN ISSUE TAG
-        </h2>
-        {articles.map((article, i) => (
-          <Aux key={i}>
-            <Link to={`/articles/${article.slug}`}>
-              <p className="row">
-                <span className="col-sm-4">{article.articleType}</span>
-                <span className="col-sm-8">
-                  {article.title} -- {article.tags.join(', ')}}
-                </span>
-              </p>
-            </Link>
-          </Aux>
-        ))}
+        <h2>{this.props.match.params.slug}</h2>
+        {articles.map((article, i) => {          
+          let articleThumbnail = article.thumbnail
+            ? article.thumbnail
+            : 'https://placekitten.com/400/300';
+          return (
+            <ArticleLink
+              key={`${article.slug}`}
+              article={article}
+              slug={article.slug}
+              imageSrc={articleThumbnail}
+              title={article.title}
+              articleType={article.articleType}
+              author={article.author}
+              updatedAt={article.updatedAt}
+              tagRoute="research"
+              tags={article.tags}
+            />
+          );
+        })}
       </div>
     );
   }
