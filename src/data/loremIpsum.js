498b30d28038d6ff0ae8b2f808b7753b6a0d4619
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-19 16:37:42 -0700

    loremIpsum: filler text and some formatting for populating components lacking real content

---
+++ src/data/loremIpsum.js
@@ -0,0 +1,26 @@
+const loremIpsum = [{
+  format: 'title', content: "Lorem Ipsum"
+},{
+  format: 'subtitle', 
+  content: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+},{
+  format: 'subtitle', 
+  content: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+},{
+  format: 'text', 
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien dui, rutrum a enim sed, suscipit tempor nisl. Vivamus ultrices, sem nec imperdiet rhoncus, velit leo dictum mauris, id volutpat orci velit eget metus. Pellentesque feugiat a tellus ac iaculis. Suspendisse ullamcorper hendrerit porttitor. Aliquam eleifend enim et semper vehicula. Nam eu maximus metus. Nullam leo magna, varius eget est in, sodales scelerisque mauris. Vestibulum quis facilisis ante. Nulla tempus tincidunt tortor id volutpat. In sit amet enim ac sem egestas suscipit in at tortor. Curabitur ornare fermentum lorem eu cursus. Phasellus eros turpis, dapibus vitae porta quis, bibendum sed lectus. Nulla a pharetra mauris. Nam nec magna metus. Sed semper dapibus leo. Donec lobortis metus at leo malesuada tempor."
+},{
+  format: 'text', 
+  content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce luctus facilisis condimentum. Quisque bibendum cursus eros, a varius magna. Suspendisse sem arcu, rutrum ut lacus ut, tincidunt tincidunt nulla. Aliquam posuere eget urna non posuere. Nam commodo pharetra enim sed congue. Aenean convallis sapien metus. Mauris et nisl tristique, facilisis arcu quis, luctus justo. Mauris id eros efficitur, mattis nibh ac, aliquet nibh. Suspendisse feugiat lobortis varius. Integer at sagittis mi, ut hendrerit eros. Nulla sed venenatis augue. Vestibulum sit amet purus euismod, ultrices arcu et, scelerisque metus. Sed ac turpis pharetra, mollis ex vel, feugiat neque."
+},{
+  format: 'text', 
+  content: "Mauris in risus ut ex venenatis luctus at at erat. Sed dignissim, odio pharetra vestibulum facilisis, nunc dolor sagittis nisl, ut sodales neque sem quis ligula. Praesent nisl diam, fermentum quis suscipit id, eleifend id purus. Ut lacinia orci sed turpis hendrerit, et ultrices mauris posuere. Vestibulum vehicula nunc quis augue faucibus, nec dapibus nisi eleifend. Nulla mi tellus, malesuada in pellentesque ut, venenatis nec nulla. Donec iaculis enim et nisi placerat tempus. Morbi efficitur elementum arcu at interdum."
+},{
+  format: 'text', 
+  content: "Proin eget mattis dolor, ac mollis lectus. Donec varius, nisl eu sodales mattis, nulla quam tincidunt ante, nec hendrerit purus neque nec dui. Etiam quis metus lorem. Aenean tincidunt volutpat eros, ut volutpat arcu. Proin fringilla urna et urna interdum tempor. Nunc libero justo, blandit sed posuere eu, suscipit sed lorem. Nulla non ipsum egestas, ultrices odio non, consectetur velit. Nullam id orci massa. Suspendisse pretium egestas mauris ac lacinia. Maecenas lacinia arcu non tincidunt posuere. Curabitur dapibus massa sem, a ornare ligula tempor at. Donec sodales maximus velit, at egestas libero finibus sed. Etiam porttitor lectus quis tortor condimentum egestas. Nullam sit amet accumsan nisl."
+},{
+  format: 'text', 
+  content: "Donec ac ullamcorper magna. Vestibulum fringilla arcu nec ex fermentum, in vulputate magna blandit. Sed vulputate justo sit amet congue porttitor. Ut nec turpis commodo, laoreet libero sit amet, faucibus ligula. Maecenas hendrerit sem vitae sollicitudin iaculis. Ut blandit tincidunt venenatis. Sed turpis ligula, luctus ut tellus nec, auctor pulvinar diam. Quisque ac lorem pretium, efficitur urna sed, venenatis tellus. Curabitur accumsan nulla purus, ut rutrum ex tincidunt nec. Nullam eu lacinia felis. Pellentesque semper a purus id lobortis. Nullam dignissim nibh sit amet feugiat sodales. Suspendisse imperdiet est vel urna dictum vestibulum. Donec maximus tortor sapien, id venenatis sem porttitor euismod. Donec fringilla justo nec justo euismod consectetur. Nullam tempus tellus odio, et tincidunt dui sodales interdum."
+}];
+
+export default loremIpsum;
