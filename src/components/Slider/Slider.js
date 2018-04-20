fddffde646d2d2c1bbfa46352132dbdf1db92692
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-20 15:25:28 -0700

    Slider: created Slider stub

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Slider = () => (
+  <div className="row">
+    <section 
+    	className="col-12"
+     	style={{
+     		height: "300px", 
+     		backgroundColor: 'aliceblue',
+     		textAlign: 'center',
+     	}}
+     >
+			<h2>Slider here</h2>
+    </section>
+  </div>
+);
+
+export default Slider;
