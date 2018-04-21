c62cedba084453fc513b30047dcaa99771b98dea
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-20 18:15:00 -0700

    States: corrected row/col nesting to work with changes to Home

--- src/components/States/States.js
+++ src/components/States/States.js
@@ -5,23 +5,21 @@ import CardHover from '../common/Cards/CardHover';
 import FooterCard from '../common/Cards/FooterCard';
 
 const States = ({ currentStateRaces }) => (
-  <section className="col-12">
-    <h2 className="text-center">Our States</h2>
-    <div className="row">
-      {currentStateRaces.map((state, i) => (
-        <GridXSmallIsOneSmallIsThree key={i}>
-          <CardHover>
-            <Link to={`./districts/${state}`}>
-              <FooterCard
-                cardTitle={state}
-                imgSrc={`../images/${state}.jpg`}
-                insetImg="insetImg"
-              />
-            </Link>
-          </CardHover>
-        </GridXSmallIsOneSmallIsThree>
-      ))}
-    </div>
+  <section className="row">
+    <h2 className="text-center col-12">Our States</h2>
+    {currentStateRaces.map((state, i) => (
+      <GridXSmallIsOneSmallIsThree key={i}>
+        <CardHover>
+          <Link to={`./districts/${state}`}>
+            <FooterCard
+              cardTitle={state}
+              imgSrc={`../images/${state}.jpg`}
+              insetImg="insetImg"
+            />
+          </Link>
+        </CardHover>
+      </GridXSmallIsOneSmallIsThree>
+    ))}
   </section>
 );
 
