9846e1d0f86ba47f3e1102d1f5db9e0347443cdc
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-22 03:47:38 -0700

    Home: passed slides to updated Slider (Carousel), autolinting by prettier

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import States from '../States/States';
 import Slider from '../common/Sliders/Slider';
+import Parallax from '../common/Parallax/Parallax';
 import CandidatesFaces from '../common/CandidatesFaces';
 import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import CardHover from '../common/Cards/CardHover';
@@ -19,15 +20,40 @@ const Home = props => {
   return (
     <div className="Home">
 
-      <Slider />
+      <Slider
+        slides={[
+          {
+            src: './images/local_majority_banner.png',
+            alt: 'local majority logo',
+          },
+          {
+            src: 'https://placekitten.com/900/300',
+            alt: 'place kitten 900 by 300',
+          },
+          {
+            src: './images/local_majority_banner.png',
+            alt: 'local majority logo',
+          },
+          {
+            src: 'https://placekitten.com/600/200',
+            alt: 'place kitten 600  by 200',
+          },
+        ]}
+      />
 
       <section className="row">
         <div className="col-12">
           <h2 className="text-center">
-            State Legislatures are <span className="tertiary-text-color">Key</span>
+            State Legislatures are{' '}
+            <span className="tertiary-text-color">Key</span>
           </h2>
           <p className="text-center">
-            State legislatures not only create the laws that affect the everyday lives of their constituents, they also play a critical role in determining the districts for the U.S. House of Representatives and thus the fate of the entire country. Local Majority provides research that is practical and accessible to support progressive campaigns for state legislature. Join us or support our efforts!
+            State legislatures not only create the laws that affect the everyday
+            lives of their constituents, they also play a critical role in
+            determining the districts for the U.S. House of Representatives and
+            thus the fate of the entire country. Local Majority provides
+            research that is practical and accessible to support progressive
+            campaigns for state legislature. Join us or support our efforts!
           </p>
         </div>
       </section>
@@ -38,7 +64,8 @@ const Home = props => {
             Our Latest <span className="tertiary-text-color">Reports</span>
           </h2>
           <p className="text-center">
-            Read <Link to="./reports">here</Link> for our latest in-depth research reports supporting progressive state district campaigns.
+            Read <Link to="./reports">here</Link> for our latest in-depth
+            research reports supporting progressive state district campaigns.
           </p>
           <div className="row">
             {articles && articles.length ? (
@@ -76,7 +103,10 @@ const Home = props => {
           <h2 className="text-center">
             Our Featured <span className="tertiary-text-color">Candidates</span>
           </h2>
-          <p className="text-center">Click <Link to="./candidates">here</Link> for all the progressive state district campaigns we are supporting with our research.</p>
+          <p className="text-center">
+            Click <Link to="./candidates">here</Link> for all the progressive
+            state district campaigns we are supporting with our research.
+          </p>
           <div className="row">
             {candidates && candidates.length ? (
               candidates.map((candidate, i) => {
@@ -117,13 +147,41 @@ const Home = props => {
         </div>
       </section>
 
-
-      <Slider />
+      <Slider
+        slides={[
+          {
+            src: './images/local_majority_banner.png',
+            alt: 'local majority logo',
+          },
+          {
+            src: 'https://placekitten.com/900/300',
+            alt: 'place kitten 900 by 300',
+          },
+          {
+            src: 'https://placekitten.com/600/200',
+            alt: 'place kitten 600  by 200',
+          },
+          {
+            src: './images/local_majority_banner.png',
+            alt: 'local majority logo',
+          },
+          {
+            src: 'https://placekitten.com/1200/300',
+            alt: 'place kitten 1200 by 300',
+          },
+          {
+            src: 'https://placekitten.com/600/300',
+            alt: 'place kitten 600 by 300',
+          },
+        ]}
+      />
 
       <CandidatesFaces
         candidatesVA2017={candidatesVA2017}
         candidates={candidates}
       />
+
+      <Parallax />
 
       <section>
         <div className="row">
@@ -140,7 +198,9 @@ const Home = props => {
               Saving our <span className="tertiary-text-color">Democracy</span>
             </h2>
             <p className="text-center">It's time to flip the country blue</p>
-            <p className="text-center">Help us take back state legislatures in November.</p>
+            <p className="text-center">
+              Help us take back state legislatures in November.
+            </p>
             <p className="text-center">BLUE IN 2018!</p>
           </div>
         </div>
@@ -152,15 +212,19 @@ const Home = props => {
             Take Action <span className="tertiary-text-color">Now!</span>
           </h2>
           <p className="text-center">Register to Vote</p>
-          <p className="text-center">Check your Voter Registration is Current</p>
+          <p className="text-center">
+            Check your Voter Registration is Current
+          </p>
           <p className="text-center">Register to Vote by Mail</p>
           <p className="text-center">Reach out to People from your State</p>
           <p className="text-center">Volunteer to Get out the Vote</p>
           <p className="text-center">Donate to Progressive Candidates</p>
-          <p className="text-center">Click <Link to="./take-action">here</Link> to make a difference in 2018.</p>
+          <p className="text-center">
+            Click <Link to="./take-action">here</Link> to make a difference in
+            2018.
+          </p>
         </div>
       </section>
-
     </div>
   );
 };
