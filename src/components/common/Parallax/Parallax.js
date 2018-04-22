dbd3a38759c318bb1b797b4b49256605a7359818
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-22 01:25:41 -0700

    Parallax: created stub for Parallax component

---
+++ src/components/common/Parallax/Parallax.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Parallax = () => (
+	<div className='Parallax row'>
+		<section
+			className='col-12'
+			style={{
+				height: '300px',
+				backgroundColor: 'aliceblue',
+				textAlign: 'center',
+			}}
+		>
+			<h2>Parallax here</h2>
+		</section>
+	</div>
+)
+
+export default Parallax;
