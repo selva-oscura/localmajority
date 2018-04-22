200cb1c09ff24f949f189157ab3fd0c0c2682978
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-22 03:28:07 -0700

    Slider: inserted boilerplate from react-responsive-carousel

--- src/components/common/Sliders/Slider.js
+++ src/components/common/Sliders/Slider.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+// general styles
+import 'react-responsive-carousel/lib/styles/main.css';
+// carousel styles
+import 'react-responsive-carousel/lib/styles/carousel.css';
 
 const Slider = () => (
   <div className="row">
-    <section 
-    	className="col-12"
-     	style={{
-     		height: "300px", 
-     		backgroundColor: 'aliceblue',
-     		textAlign: 'center',
-     	}}
-     >
-			<h2>Slider here</h2>
+    <section className="col-12">
+      <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
+        <div>
+          <img src="assets/1.jpeg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="assets/2.jpeg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="assets/4.jpeg" />
+          <p className="legend">Legend 4</p>
+        </div>
+        <div>
+          <img src="assets/5.jpeg" />
+          <p className="legend">Legend 5</p>
+        </div>
+        <div>
+          <img src="assets/6.jpeg" />
+          <p className="legend">Legend 6</p>
+        </div>
+      </Carousel>
     </section>
   </div>
 );
