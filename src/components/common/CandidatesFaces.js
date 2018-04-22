de4d4d28bab9d661d478d3c52a32b8044ff22adf
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-22 03:48:16 -0700

    CandidateFaces: autolinting by prettier

--- src/components/common/CandidatesFaces.js
+++ src/components/common/CandidatesFaces.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CandidatesFaces = ({candidatesVA2017, candidates}) => {
+const CandidatesFaces = ({ candidatesVA2017, candidates }) => {
   const facesCandidates = []
     // pastCandidates
     //   .map(candidate => {
@@ -48,55 +48,50 @@ const CandidatesFaces = ({candidatesVA2017, candidates}) => {
     );
   }
 
-
-	return (
-		<section>	
-		  <div className="FacesColourBackground" style={{ padding: '24px 0' }}>
-		    {presentationForCandidatesFaces.map((rowContents, i) => (
-		      <div className="row no-gutters" key={i}>
-		        {rowContents.map((candidate, j) => {
-		          const imgSrc = candidate.headshot
-		            ? candidate.headshot
-		            : `../images/candidates_of_yore/color/${
-		                candidate.slug
-		              }.png`;
-		          return (
-		            <div
-		              className="col"
-		              key={j}
-		              style={{
-		                padding: '1px',
-		                background:
-		                  "url('images/placeholderImage.svg') no-repeat",
-		                backgroundSize: '100%',
-		                backgroundPosition: 'center 0',
-		                overflow: 'hidden',
-		              }}
-		            >
-		              <img
-		                className="full-width"
-		                src={imgSrc}
-		                alt={`${candidate.title}, Local Majority backed ${
-		                  candidate.electionYear
-		                } candidate for ${candidate.district}`}
-		                title={`${candidate.title}, Local Majority backed ${
-		                  candidate.electionYear
-		                } candidate for ${candidate.district}`}
-		                style={{
-		                  textIndent: '100%',
-		                  whiteSpace: 'nowrap',
-		                  overflow: 'hidden',
-		                }}
-		              />
-		            </div>
-		          );
-		        })}
-		      </div>
-		    ))}
-		  </div>
-		</section>
-	);
-
-}
+  return (
+    <section>
+      <div className="FacesColourBackground" style={{ padding: '24px 0' }}>
+        {presentationForCandidatesFaces.map((rowContents, i) => (
+          <div className="row no-gutters" key={i}>
+            {rowContents.map((candidate, j) => {
+              const imgSrc = candidate.headshot
+                ? candidate.headshot
+                : `../images/candidates_of_yore/color/${candidate.slug}.png`;
+              return (
+                <div
+                  className="col"
+                  key={j}
+                  style={{
+                    padding: '1px',
+                    background: "url('images/placeholderImage.svg') no-repeat",
+                    backgroundSize: '100%',
+                    backgroundPosition: 'center 0',
+                    overflow: 'hidden',
+                  }}
+                >
+                  <img
+                    className="full-width"
+                    src={imgSrc}
+                    alt={`${candidate.title}, Local Majority backed ${
+                      candidate.electionYear
+                    } candidate for ${candidate.district}`}
+                    title={`${candidate.title}, Local Majority backed ${
+                      candidate.electionYear
+                    } candidate for ${candidate.district}`}
+                    style={{
+                      textIndent: '100%',
+                      whiteSpace: 'nowrap',
+                      overflow: 'hidden',
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default CandidatesFaces;
