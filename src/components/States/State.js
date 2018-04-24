a8d6e2c301ab80cd9c42b3f29afde621ee77d43e
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-23 21:54:20 -0700

    State: converted from stateless to stateful component

--- src/components/States/State.js
+++ src/components/States/State.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import BasicCard from '../common/Cards/BasicCard';
 
-const State = ({ state, candidates, reports }) => {
-  console.log('state', state);
-  console.log('candidates', candidates);
-  console.log('reports', reports);
-  return (
-    <section className="State">
-      <div className="container">
-        <div className="row">
-          <h2 className="text-center col-12">
-            Our State Info <span className="tertiary-text-color">Here</span>
-          </h2>
+class State extends Component {
+
+  render(){
+    const { state, candidates, reports } = this.props;
+    console.log('state', state);
+    console.log('candidates', candidates);
+    console.log('reports', reports);
+    return (
+      <section className="State">
+        <div className="container">
+          <div className="row">
+            <h2 className="text-center col-12">
+              {state.title} Info <span className="tertiary-text-color">Here</span>
+            </h2>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-};
+      </section>
+    )
+
+  }
+}
 
 export default State;
-        // {currentStateRaces.map((state, i) => (
-        //   <GridXSmallIsOneSmallIsThree key={i}>
-        //     <BasicCard
-        //       title={state}
-        //       text=""
-        //       route="states"
-        //       slug={state}
-        //       imageSrc={`../images/${state}.jpg`}
-        //       imageAlt={state}
-        //     />
-        //   </GridXSmallIsOneSmallIsThree>
-        // ))}
