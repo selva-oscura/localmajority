8bcb1034864a14e5ff822fb5ee97195be5334db3
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-24 21:56:19 -0700

    Issue: added container class to these Issue

--- src/components/Issues/Issue.js
+++ src/components/Issues/Issue.js
@@ -19,17 +19,49 @@ class Issue extends Component {
 
     return issue ? (
       <main className="Issue">
-        <h2>{issue.title}</h2>
-        {subIssues.map(subIssue => (
-          <Aux key={subIssue.title}>
-            <h4>{subIssue.title} is here</h4>
-            <div className="row">
-            {articles.map(article => {
+        <section className="container">
+          <h2>{issue.title}</h2>
+          {subIssues.map(subIssue => (
+            <Aux key={subIssue.title}>
+              <h4>{subIssue.title} is here</h4>
+              <div className="row">
+              {articles.map(article => {
+                let articleThumbnail = article.thumbnail
+                  ? article.thumbnail
+                  : 'https://placekitten.com/400/300';
+                let cardTags = article && article.tags ? article.tags : [];
+                return article.tags.includes(subIssue.slug) ? (
+                  <GridXSmallIsOneSmallIsThree key={article.slug}>
+                    <ArticleCard
+                      article={article}
+                      slug={article.slug}
+                      imageSrc={articleThumbnail}
+                      title={article.title}
+                      author={article.author}
+                      articleType={article.articleType}
+                      updatedAt={article.updatedAt}
+                      tagRoute="reports"
+                      tags={cardTags}
+                    />
+                  </GridXSmallIsOneSmallIsThree>
+                ) : null;
+              })}
+              </div>
+            </Aux>
+          ))}
+        </section>
+      </main>
+    ) : (
+      <main className="Issue">
+        <section className="container">
+          <h2>{this.props.match.params.slug}</h2>
+          <div className="row">
+            {articles.map((article, i) => {
               let articleThumbnail = article.thumbnail
                 ? article.thumbnail
                 : 'https://placekitten.com/400/300';
               let cardTags = article && article.tags ? article.tags : [];
-              return article.tags.includes(subIssue.slug) ? (
+              return (
                 <GridXSmallIsOneSmallIsThree key={article.slug}>
                   <ArticleCard
                     article={article}
@@ -43,38 +75,10 @@ class Issue extends Component {
                     tags={cardTags}
                   />
                 </GridXSmallIsOneSmallIsThree>
-              ) : null;
+              );
             })}
-            </div>
-          </Aux>
-        ))}
-      </main>
-    ) : (
-      <main className="Issue">
-        <h2>{this.props.match.params.slug}</h2>
-        <div className="row">
-          {articles.map((article, i) => {
-            let articleThumbnail = article.thumbnail
-              ? article.thumbnail
-              : 'https://placekitten.com/400/300';
-            let cardTags = article && article.tags ? article.tags : [];
-            return (
-              <GridXSmallIsOneSmallIsThree key={article.slug}>
-                <ArticleCard
-                  article={article}
-                  slug={article.slug}
-                  imageSrc={articleThumbnail}
-                  title={article.title}
-                  author={article.author}
-                  articleType={article.articleType}
-                  updatedAt={article.updatedAt}
-                  tagRoute="reports"
-                  tags={cardTags}
-                />
-              </GridXSmallIsOneSmallIsThree>
-            );
-          })}
-        </div>
+          </div>
+        </section>
       </main>
     );
   }
