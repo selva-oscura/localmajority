d30893e2e865ff562dddd1a045deefde42198ef8
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-04-28 18:46:35 -0700

    TextlessParallax: replaced stub Parallax component with a textless react-parallax component

---
+++ src/components/common/Parallax/TextlessParallax.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Parallax } from 'react-parallax';
+
+const TextlessParallax = ({imgSrc="/images/constitution.jpg", strength="500", height="500"}) => (
+  <Parallax bgImage={imgSrc}
+    strength={Number(strength)}>
+    <div style={{height: Number(height)}}></div>
+  </Parallax>
+)
+
+export default TextlessParallax;
