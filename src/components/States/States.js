e9324b8e8461472369458d78a360d1f5667c3a87
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-01 00:45:02 -0700

    States: changing of bootstrap classes, due to container being removed from App, removed console.log

--- src/components/States/States.js
+++ src/components/States/States.js
@@ -2,35 +2,28 @@ import React from 'react';
 import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import BasicCard from '../common/Cards/BasicCard';
 
-const States = ({ currentStateRaces }) => {
-  console.log('currentStateRaces', currentStateRaces);
-  return (
-    <section className="States">
-      <div className="container">
-        <div className="row">
-          <h2 className="text-center col-12">
-            Our <span className="tertiary-text-color">States</span>
-          </h2>
-          <p className="text-center">
-            We are supporting states where we can make a big difference and help
-            flip seats blue in State Houses and Senate races.
-          </p>
-          {currentStateRaces.map((state, i) => (
-            <GridXSmallIsOneSmallIsThree key={i}>
-              <BasicCard
-                title={state.title}
-                text={state.text}
-                route="states"
-                slug={state.title}
-                imageSrc={`../images/${state.title}.jpg`}
-                imageAlt={state.title}
-              />
-            </GridXSmallIsOneSmallIsThree>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
+const States = ({ currentStateRaces }) => (
+  <div className="row">
+    <h2 className="text-center col-12">
+      Our <span className="tertiary-text-color">States</span>
+    </h2>
+    <p className="text-center">
+      We are supporting states where we can make a big difference and help
+      flip seats blue in State Houses and Senate races.
+    </p>
+    {currentStateRaces.map((state, i) => (
+      <GridXSmallIsOneSmallIsThree key={i}>
+        <BasicCard
+          title={state.title}
+          text={state.text}
+          route="states"
+          slug={state.title}
+          imageSrc={`../images/${state.title}.jpg`}
+          imageAlt={state.title}
+        />
+      </GridXSmallIsOneSmallIsThree>
+    ))}
+  </div>
+);
 
 export default States;
