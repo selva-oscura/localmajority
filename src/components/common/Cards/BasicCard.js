8cf66e8fba53a6f2e688e371fb199010fa8cfd21
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-01 00:46:41 -0700

    BasicCard: removed CardHover effect

--- src/components/common/Cards/BasicCard.js
+++ src/components/common/Cards/BasicCard.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ImageWithBackgroundPlaceholderImage from '../ImageWithBackgroundPlaceholderImage';
-import CardHover from './CardHover';
 import './BasicCard.css';
 
 const BasicCard = ({title="", subtitle, text="", route="", slug="", imageSrc="", aspectRatio="100", }) => (
   <Link
     to={`/${route}/${slug}`}
   >
-	  <CardHover>
-  		<div className="BasicCard">
-	      <ImageWithBackgroundPlaceholderImage
-	        imageSrc={imageSrc}
-	        imageAlt={title}
-	        aspectRatioInPercent={aspectRatio}
-	      />
-	      <div className="contents">
-		      <h3 className="title">{title}</h3>
-		      {subtitle && <h4>{subtitle}</h4>}
-		      {text && <p>{text}</p>}
-	      </div>
-	   </div>
-  	</CardHover>
+		<div className="BasicCard">
+		  <ImageWithBackgroundPlaceholderImage
+		    imageSrc={imageSrc}
+		    imageAlt={title}
+		    aspectRatioInPercent={aspectRatio}
+		  />
+		  <div className="contents">
+		    <h3 className="title">{title}</h3>
+		    {subtitle && <h4>{subtitle}</h4>}
+		    {text && <p>{text}</p>}
+		  </div>
+		</div>
 	</Link>
 );
 
