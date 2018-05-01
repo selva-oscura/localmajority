fbe600c509427e194e81d67fca7e7ddab073f862
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-01 00:49:07 -0700

    ArticleCard: removed CardHover effect

--- src/components/common/Cards/ArticleCard.js
+++ src/components/common/Cards/ArticleCard.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ImageWithBackgroundPlaceholderImage from '../ImageWithBackgroundPlaceholderImage';
-import CardHover from './CardHover';
 import { prettifyDate } from '../../../utils/functions';
 import './ArticleCard.css';
 
 const ArticleCard = ({slug, imageSrc, imageAlt="", title, author, updatedAt, tagRoute="", tags=[]}) => (
-  <CardHover>
-  	<div className="ArticleCard">
-	    <Link
-	      to={`/articles/${slug}`}
-	    >
-	      <ImageWithBackgroundPlaceholderImage
-	        imageSrc={imageSrc}
-	        imageAlt=""
-	        aspectRatioInPercent="75"
-	      />
-	      <div className="contents">
-		      <h3 className="title">{title}</h3>
-		      <p className="details">{author}</p>
-		      <p className="details text-right">updated {prettifyDate(updatedAt)}</p>
-	      </div>
-	    </Link>
-	    {tags && tags.length
-				? (
-						<div className="tags flex-container flex-wrap flex-start">
-							{tags.map(tag => (
-								<Link key={tag} to={`/${tagRoute}/${tag}`} className="tag">{tag}
-								</Link>
-							))}
-						</div>
-				)
-	    	: null
-	    }
-	   </div>
-  </CardHover>
+	<div className="ArticleCard">
+    <Link
+      to={`/articles/${slug}`}
+    >
+      <ImageWithBackgroundPlaceholderImage
+        imageSrc={imageSrc}
+        imageAlt=""
+        aspectRatioInPercent="75"
+      />
+      <div className="contents">
+	      <h3 className="title">{title}</h3>
+	      <p className="details">{author}</p>
+	      <p className="details text-right">updated {prettifyDate(updatedAt)}</p>
+      </div>
+    </Link>
+    {tags && tags.length
+			? (
+					<div className="tags flex-container flex-wrap flex-start">
+						{tags.map(tag => (
+							<Link key={tag} to={`/${tagRoute}/${tag}`} className="tag">{tag}
+							</Link>
+						))}
+					</div>
+			)
+      : null
+    }
+   </div>
 );
 
 export default ArticleCard;
