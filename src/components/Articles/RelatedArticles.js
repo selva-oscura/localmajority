57b0fc62a6372b2de3045b575952a01125563a28
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-01 00:40:39 -0700

    FeaturedArticles: created standalone component for related articles

---
+++ src/components/Articles/RelatedArticles.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
+import ArticleCard from '../common/Cards/ArticleCard';
+
+const RelatedArticles = ({articles}) => (
+  <aside className="row">
+    <h3 className="col-12">Related Reports</h3>
+    {articles && articles.map(article => {
+      let cardTags = article && article.tags ? article.tags : [];
+      let articleThumbnail = article.thumbnail
+        ? article.thumbnail
+        : 'https://placekitten.com/g/200/150';
+      return (
+        <GridXSmallIsOneSmallIsThree key={article.slug}>
+          <ArticleCard
+            slug={article.slug}
+            imageSrc={articleThumbnail}
+            title={article.title}
+            author={article.author}
+            updatedAt={article.updatedAt}
+            tagRoute="reports"
+            tags={cardTags}
+          />
+        </GridXSmallIsOneSmallIsThree>
+      )})}
+  </aside>
+);
+
+export default RelatedArticles;
