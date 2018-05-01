c8988a284d7f72596f05d48ce56a7079cbf430d9
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-01 01:17:40 -0700

    Section: moved component to own directory

---
+++ src/components/common/Section/Section.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Section = (props) => {
+	let { background, hasContainer, spacingAbove, spacingBelow, } = props;
+	let backgroundColor;
+	if (background === "light") {
+		backgroundColor = "background-light";
+	} else if (background === "medium") {
+		backgroundColor = "background-medium";
+	} else if (background === "dark") {
+		backgroundColor = "background-dark";
+	} else {
+		backgroundColor = "";
+	}
+
+	let spacingDefault = 12;
+	let marginTop = spacingAbove && Number(spacingAbove)
+		? spacingDefault*spacingAbove + "px"
+		: "0px";
+	let marginBottom = spacingBelow && Number(spacingBelow)
+		? spacingDefault*spacingBelow + "px"
+		: "0px";
+	let styles = { marginTop, marginBottom };
+
+	return (
+		<section className={backgroundColor}>
+			{hasContainer 
+				? (
+					<div className="container" style={styles}>
+						{props.children}
+					</div>
+				) : (
+					<div style={styles}>
+						{props.children}
+					</div>
+				)
+			}
+		</section>
+	);
+}
+
+export default Section;
