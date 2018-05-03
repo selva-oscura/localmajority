8201a8f324923335a1a6a4536484f93f76618212
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-02 17:47:17 -0700

    Home: switched larger Parallax to constitution image, decreased height, increased strength

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -163,9 +163,9 @@ const Home = props => {
 
       <Section hasContainer={false}>
         <TextlessParallax
-          imgSrc="https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-          strength="500"
-          height="500"
+          imgSrc="/images/constitution.jpg"
+          strength="800"
+          height="400"
         />
       </Section>
 
