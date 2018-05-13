4da68f7b4bf23739b9431a969f03c466b360621d
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-12 22:22:15 -0700

    ImageWithBackgroundPlaceholderImage: added pale grey background color to fill full space image will occupy

--- src/components/common/ImageWithBackgroundPlaceholderImage.js
+++ src/components/common/ImageWithBackgroundPlaceholderImage.js
@@ -4,8 +4,8 @@ import React from 'react';
 	NOTE: aspectRatioInPercent: (width/height)*100
 	Square is 100
 	16 (width) by 9 (height) is 56.25
-	4 (width) by 3(height) is 75
-  quick tip to check for eyeballing it is right:
+	4 (width) by 3 (height) is 75
+  quick tip for eyeballing it is right:
     horizontal < 100, vertical > 100
 */
 
@@ -15,31 +15,33 @@ const ImageWithBackgroundPlaceholderImage = ({
   backgroundImageURL = '/images/placeholderImage.svg',
   aspectRatioInPercent = 100,
 }) => (
-  <div
-    style={{
-      paddingBottom: '0',
-      width: '100%',
-      paddingTop: `${aspectRatioInPercent}%`,
-      background: `url(${backgroundImageURL}) no-repeat`,
-      backgroundSize: `${aspectRatioInPercent}%`,
-      backgroundPosition: 'center center',
-      overflow: 'hidden',
-      lineHeight: 0,
-    }}
-  >
-    <img
-      src={imageSrc}
+  <div style={{backgroundColor: 'rgba(238,238,238, 0.5)'}}>
+    <div
       style={{
-        marginTop: `-${aspectRatioInPercent}%`,
+        paddingBottom: '0',
         width: '100%',
-        maxWidth: '100%',
-        textIndent: '100%',
-        whiteSpace: 'nowrap',
+        paddingTop: `${aspectRatioInPercent}%`,
+        background: `url(${backgroundImageURL}) no-repeat`,
+        backgroundSize: `${aspectRatioInPercent}%`,
+        backgroundPosition: 'center center',
         overflow: 'hidden',
+        lineHeight: 0,
       }}
-      alt={imageAlt}
-      title={imageAlt}
-    />
+    >
+      <img
+        src={imageSrc}
+        style={{
+          marginTop: `-${aspectRatioInPercent}%`,
+          width: '100%',
+          maxWidth: '100%',
+          textIndent: '100%',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+        }}
+        alt={imageAlt}
+        title={imageAlt}
+      />
+    </div>
   </div>
 );
 
