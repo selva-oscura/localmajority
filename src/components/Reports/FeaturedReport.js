d0e60f15a51616d48813500bb809f49edafa11db
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-15 21:26:44 -0700

    ArticleCard, FeaturedReport: added light box-shadow on hover

--- src/components/Reports/FeaturedReport.js
+++ src/components/Reports/FeaturedReport.js
@@ -19,21 +19,23 @@ const FeaturedReport = ({ article }) => {
   return (
     <div className="FeaturedReport row">
       <div className="col-12" style={{ margin: '1vw 0px 2vw 0px' }}>
-        <Link to={`/report/${slug}`}>
-          <ImageWithBackgroundPlaceholderImage
-            imageSrc={imageSrc}
-            imageAlt=""
-            aspectRatioInPercent="25"
-          />
-          <div className="contents row">
-            <h3 className="title col-12">{title}</h3>
-            <p className="details col-12 col-sm-6">{author}</p>
-            <p className="details col-12 col-sm-6 text-right">
-              updated {prettifyDate(updatedAt)}
-            </p>
-          </div>
-        </Link>
-        {tags && tags.length ? <Tags tags={tags} tagRoute={tagRoute} /> : null}
+        <div className="FeaturedReportShadow">
+          <Link to={`/report/${slug}`}>
+            <ImageWithBackgroundPlaceholderImage
+              imageSrc={imageSrc}
+              imageAlt=""
+              aspectRatioInPercent="25"
+            />
+            <div className="contents row">
+              <h3 className="title col-12">{title}</h3>
+              <p className="details col-12 col-sm-6">{author}</p>
+              <p className="details col-12 col-sm-6 text-right">
+                updated {prettifyDate(updatedAt)}
+              </p>
+            </div>
+          </Link>
+          {tags && tags.length ? <Tags tags={tags} tagRoute={tagRoute} /> : null}
+        </div>
       </div>
     </div>
   );
