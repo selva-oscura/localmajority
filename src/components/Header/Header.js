0d23392096b103a4db4957cb79eb115f17bdd515
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-16 18:58:02 -0700

    Header: fixed onLeftIconButtonTouchTap bug after update of material-ui package

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
                 />
               </IconButton>
             }
-            onLeftIconButtonTouchTap={this.handleHamburgerClick}
+            onLeftIconButtonClick={this.handleHamburgerClick}
           />
           <NavDrawer
             navLinks={navLinks}
