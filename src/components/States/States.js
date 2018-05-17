ecadd007be11ed2f973699bd74b35be6afddc0be
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-16 22:50:09 -0700

    States: fixed extra spacing-at-top/page-title-not-aligning-with-other-page-titles problem

--- src/components/States/States.js
+++ src/components/States/States.js
@@ -3,27 +3,29 @@ import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsT
 import BasicCard from '../common/Cards/BasicCard';
 
 const States = ({ currentStateRaces }) => (
-  <div className="row">
-    <h2 className="text-center col-12">
+  <div>
+    <h2 className="text-center">
       Our <span className="tertiary-text-color">States</span>
     </h2>
-    <p className="text-center col-12">
+    <p className="text-center">
       Local Majority is supporting progressive candidates in State House and
       State Senate races in purple states where flipping seats blue can make the
       biggest difference.
     </p>
-    {currentStateRaces.map((state, i) => (
-      <GridXSmallIsOneSmallIsThree key={i}>
-        <BasicCard
-          title={state.title}
-          text={state.text}
-          route="states"
-          slug={state.title}
-          imageSrc={`../images/${state.title}.jpg`}
-          imageAlt={state.title}
-        />
-      </GridXSmallIsOneSmallIsThree>
-    ))}
+    <div className="row">
+      {currentStateRaces.map((state, i) => (
+        <GridXSmallIsOneSmallIsThree key={i}>
+          <BasicCard
+            title={state.title}
+            text={state.text}
+            route="states"
+            slug={state.title}
+            imageSrc={`../images/${state.title}.jpg`}
+            imageAlt={state.title}
+          />
+        </GridXSmallIsOneSmallIsThree>
+      ))}
+    </div>
   </div>
 );
 
