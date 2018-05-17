f87b2456699416bd47225ee9b32316a0634ac9a1
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-16 23:13:44 -0700

    RelatedReports: removed Link import

--- src/components/Reports/RelatedReports.js
+++ src/components/Reports/RelatedReports.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import ArticleCard from '../common/Cards/ArticleCard';
 
