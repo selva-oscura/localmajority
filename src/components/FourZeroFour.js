f916a2b14d1781b68a38c2ca5ea1d64954fe5ee9
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-05-25 12:42:32 -0700

    FourZeroFour: autolinting by prettier

--- src/components/FourZeroFour.js
+++ src/components/FourZeroFour.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import Section from './common/Section/Section';
 
 class FourZeroFour extends Component {
-	componentDidMount() {
-		this.props.history.push('/');
-	}
-	render(){
-	  return (
+  componentDidMount() {
+    this.props.history.push('/');
+  }
+  render() {
+    return (
       <article className="FourZeroFour">
-		    <Section
-		      hasContainer={true}
-		      spacingAbove={3}
-		      spacingBelow={3}
-		      background="light"
-		    >
-			    <div className="row">
-			      <div className="col-12">
-			        <p>Page Not Found</p>
-			        <a href="/">Return to Home</a>
-			      </div>
-			    </div>
-		    </Section>
-	    </article>
-	  );
-	}
-};
+        <Section
+          hasContainer={true}
+          spacingAbove={3}
+          spacingBelow={3}
+          background="light"
+        >
+          <div className="row">
+            <div className="col-12">
+              <p>Page Not Found</p>
+              <a href="/">Return to Home</a>
+            </div>
+          </div>
+        </Section>
+      </article>
+    );
+  }
+}
 
 export default FourZeroFour;
