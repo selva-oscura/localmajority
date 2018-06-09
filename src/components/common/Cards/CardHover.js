bddf8411b3e5c45b88c16ad79e56568d6c8cd2ab
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-06-08 22:42:59 -0700

    CardHover: reduced hover depths from 2 (default) & 3 (hover) to 0 & 1

--- src/components/common/Cards/CardHover.js
+++ src/components/common/Cards/CardHover.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import Paper from 'material-ui/Paper';
-import PropTypes from 'prop-types';
-import './CardHover.css';
+import React, { Component } from "react";
+import Paper from "material-ui/Paper";
+import PropTypes from "prop-types";
+import "./CardHover.css";
 
 class CardHover extends Component {
   constructor(props) {
     super(props);
-    this.state = { depth: 2 };
+    this.state = { depth: 0 };
   }
-  onMouseOver = () => this.setState({ depth: 3 });
-  onMouseOut = () => this.setState({ depth: 2 });
+  onMouseOver = () => this.setState({ depth: 1 });
+  onMouseOut = () => this.setState({ depth: 0 });
   render() {
     let { imgShape } = this.props;
-    let aspectRatio = imgShape || '';
+    let aspectRatio = imgShape || "";
     return (
       <div
         className={`CardHover ${aspectRatio}`}
         onMouseOver={this.onMouseOver}
         onMouseOut={this.onMouseOut}
       >
-        <Paper zDepth={this.state.depth} style={{ height: '100%' }}>
+        <Paper zDepth={this.state.depth} style={{ height: "100%" }}>
           {this.props.children}
         </Paper>
       </div>
