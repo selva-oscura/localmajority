e3f54411f8316ea1b480f726353003a99598ade6
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-06-08 22:37:14 -0700

    ArticleCard: reinstated CardHover, added contents class around tags

--- src/components/common/Cards/ArticleCard.js
+++ src/components/common/Cards/ArticleCard.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ImageWithBackgroundPlaceholderImage from '../ImageWithBackgroundPlaceholderImage';
-import Tags from '../Tags/Tags';
-import { prettifyDate } from '../../../utils/functions';
-import './ArticleCard.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import ImageWithBackgroundPlaceholderImage from "../ImageWithBackgroundPlaceholderImage";
+import CardHover from "./CardHover";
+import Tags from "../Tags/Tags";
+import { prettifyDate } from "../../../utils/functions";
+import "./ArticleCard.css";
 
 const ArticleCard = ({
   slug,
   imageSrc,
-  imageAlt = '',
+  imageAlt = "",
   title,
   author,
   updatedAt,
-  tagRoute = '',
-  tags = [],
+  tagRoute = "",
+  tags = []
 }) => (
-  <div className="ArticleCard">
-    <Link to={`/report/${slug}`}>
-      <ImageWithBackgroundPlaceholderImage
-        imageSrc={imageSrc}
-        imageAlt=""
-        aspectRatioInPercent="75"
-      />
+  <CardHover>
+    <div className="ArticleCard">
+      <Link to={`/report/${slug}`}>
+        <ImageWithBackgroundPlaceholderImage
+          imageSrc={imageSrc}
+          imageAlt=""
+          aspectRatioInPercent="75"
+        />
+        <div className="contents">
+          <h3 className="title">{title}</h3>
+          <p className="details">{author}</p>
+          <p className="details text-right">
+            updated {prettifyDate(updatedAt)}
+          </p>
+        </div>
+      </Link>
       <div className="contents">
-        <h3 className="title">{title}</h3>
-        <p className="details">{author}</p>
-        <p className="details text-right">updated {prettifyDate(updatedAt)}</p>
+        {tags && tags.length ? <Tags tags={tags} tagRoute={tagRoute} /> : null}
       </div>
-    </Link>
-    {tags && tags.length ? <Tags tags={tags} tagRoute={tagRoute} /> : null}
-  </div>
+    </div>
+  </CardHover>
 );
 
 export default ArticleCard;
