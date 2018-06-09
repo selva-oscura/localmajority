f94f1337ba1716b9cb55c60d977c0935bf132d27
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-06-08 22:40:26 -0700

    BasicCard: reinstated CardHover

--- src/components/common/Cards/BasicCard.js
+++ src/components/common/Cards/BasicCard.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ImageWithBackgroundPlaceholderImage from '../ImageWithBackgroundPlaceholderImage';
-import './BasicCard.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import ImageWithBackgroundPlaceholderImage from "../ImageWithBackgroundPlaceholderImage";
+import CardHover from "./CardHover";
+import "./BasicCard.css";
 
 const BasicCard = ({
-  title = '',
+  title = "",
   subtitle,
-  text = '',
-  route = '',
-  slug = '',
-  imageSrc = '',
-  aspectRatio = '100',
+  text = "",
+  route = "",
+  slug = "",
+  imageSrc = "",
+  aspectRatio = "100"
 }) => (
   <Link to={`/${route}/${slug}`}>
-    <div className="BasicCard">
-      <ImageWithBackgroundPlaceholderImage
-        imageSrc={imageSrc}
-        imageAlt={title}
-        aspectRatioInPercent={aspectRatio}
-      />
-      <div className="contents">
-        <h3 className="title">{title}</h3>
-        {subtitle && <h4>{subtitle}</h4>}
-        {text && <p>{text}</p>}
+    <CardHover>
+      <div className="BasicCard">
+        <ImageWithBackgroundPlaceholderImage
+          imageSrc={imageSrc}
+          imageAlt={title}
+          aspectRatioInPercent={aspectRatio}
+        />
+        <div className="contents">
+          <h3 className="title">{title}</h3>
+          {subtitle && <h4>{subtitle}</h4>}
+          {text && <p>{text}</p>}
+        </div>
       </div>
-    </div>
+    </CardHover>
   </Link>
 );
 
