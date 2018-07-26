5a1c060ffe4321dc9d1ad1c744f00c005707572b
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-07-26 02:02:44 -0700

    Tags: update Tags to reflect API changes (tags are array of objects, not array of strings)

--- src/components/common/Tags/Tags.js
+++ src/components/common/Tags/Tags.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Tags.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { strToTitleCase } from "../../../utils/functions";
+import "./Tags.css";
 
 const Tags = ({ tags, tagRoute }) => (
   <div className="Tags flex-container flex-wrap flex-start">
     {tags.map(tag => (
-      <Link key={tag} to={`/${tagRoute}/${tag}`} className="tag">
-        {tag}
+      <Link key={tag.slug} to={`/${tagRoute}/${tag.slug}`} className="tag">
+        {strToTitleCase(tag.title)}
       </Link>
     ))}
   </div>
