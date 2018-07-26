936c1312150e3abdaaa9c7a8af761d05b3e25e13
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-07-25 23:14:40 -0700

    index: autolinting by prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
-import graphQLAPI from './api/graphQLAPI';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/bootstrap-reboot.css';
-import './styles/bootstrap-grid.css';
-import './styles/font-awesome.css';
-import './index.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import localMajorityTheme from './styles/localMajorityTheme';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import graphQLAPI from "./api/graphQLAPI";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/bootstrap-reboot.css";
+import "./styles/bootstrap-grid.css";
+import "./styles/font-awesome.css";
+import "./index.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import localMajorityTheme from "./styles/localMajorityTheme";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 // console.log('endpoints', graphQLAPI.endpoints);
 // console.log('queries', graphQLAPI.queries);
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: graphQLAPI.endpoints.graphQL }),
-  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: graphQLAPI.endpoints.graphQL
+  }),
+  cache: new InMemoryCache()
 });
 
 // console.log('Apollo Client', ApolloClient);
@@ -32,7 +34,7 @@ ReactDOM.render(
       </MuiThemeProvider>
     </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 registerServiceWorker();
