772487a9ee48bcab049e4d9815dc6bff2dd48da0
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-07-26 02:03:46 -0700

    utils/functions: added strToTitleCase function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,12 +3,12 @@ getMostRecentUpdateTimestamp
 	INPUT: data -- the resolved, successful graphql query
 	OUTPUT: the most recent updatedAt's timestamp
 */
-export function getMostRecentUpdateTimestamp(data){
+export function getMostRecentUpdateTimestamp(data) {
   let mostRecent = -Infinity;
 
   const checkObject = obj => {
     Object.keys(obj).forEach(key => {
-      if(key === "updatedAt"){
+      if (key === "updatedAt") {
         let timestamp = new Date(obj[key]).getTime();
         mostRecent = Math.max(timestamp, mostRecent);
       } else if (Array.isArray(obj[key])) {
@@ -17,23 +17,22 @@ export function getMostRecentUpdateTimestamp(data){
         checkObject(obj[key]);
       }
     });
-  }
+  };
 
   const checkArray = arr => {
     arr.forEach(val => {
-      if(Array.isArray(val)){
+      if (Array.isArray(val)) {
         checkArray(val);
       } else if (val instanceof Object) {
         checkObject(val);
       }
     });
-  }
+  };
 
   Array.isArray(data) ? checkArray(data) : checkObject(data);
 
   return mostRecent;
 }
-
 
 /*
 PRETTIFYING Dates and Times
@@ -43,38 +42,50 @@ NOTE REGARDING INPUTS:
 	dateString is the string output of new Date()
 		e.g. "2018-01-31T07:55:46.000Z"
 */
-export function prettifyDate(dateString){
-	let datetime = new Date(dateString);
-	return datetime.toLocaleDateString('en-US');
+export function prettifyDate(dateString) {
+  let datetime = new Date(dateString);
+  return datetime.toLocaleDateString("en-US");
 }
 
-export function prettifyDateAndTime(dateString){
-	let datetime = new Date(dateString);
-	return `${datetime.toLocaleDateString('en-US')}, ${datetime.toLocaleTimeString('en-US')}`;
+export function prettifyDateAndTime(dateString) {
+  let datetime = new Date(dateString);
+  return `${datetime.toLocaleDateString(
+    "en-US"
+  )}, ${datetime.toLocaleTimeString("en-US")}`;
 }
 
-export function prettifyTimestamp(timestamp){
-	let datetime = new Date(timestamp);
-	return `${datetime.toLocaleDateString('en-US')}, ${datetime.toLocaleTimeString('en-US')}`;
-};
+export function prettifyTimestamp(timestamp) {
+  let datetime = new Date(timestamp);
+  return `${datetime.toLocaleDateString(
+    "en-US"
+  )}, ${datetime.toLocaleTimeString("en-US")}`;
+}
 
-export function arrayToSentenceWithCommasAndAnd(arr){
+export function arrayToSentenceWithCommasAndAnd(arr) {
   if (arr.length === 1) {
     return arr[0];
   } else if (arr.length === 2) {
-    return arr.join(' and ');
+    return arr.join(" and ");
   } else if (arr.length > 2) {
     arr[arr.length - 1] = `and ${arr[arr.length - 1]}`;
-    return arr.join(', ');
+    return arr.join(", ");
   } else {
     return null;
   }
-};
+}
+
+export function strToTitleCase(str) {
+  return str
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
 
 export default {
-	getMostRecentUpdateTimestamp,
-	prettifyDate,
-	prettifyDateAndTime,
-	prettifyTimestamp,
-	arrayToSentenceWithCommasAndAnd,
-}
+  getMostRecentUpdateTimestamp,
+  prettifyDate,
+  prettifyDateAndTime,
+  prettifyTimestamp,
+  arrayToSentenceWithCommasAndAnd,
+  strToTitleCase
+};
