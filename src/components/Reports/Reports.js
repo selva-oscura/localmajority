78e154ae35420eec67f20abf6deeef4a257f4cb6
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-08-14 23:11:10 -0700

    Reports: updated to reflect new API

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -6,10 +6,11 @@ import Aux from '../common/Aux';
 import Section from '../common/Section/Section';
 import Filters from '../common/Filters/Filters';
 import ButtonlessFilters from '../common/Filters/ButtonlessFilters';
+import { slugToTitleCase } from '../../utils/functions';
 
 class Reports extends Component {
   state = {
-    issueSelected: this.props.match.params.slug,
+    tagSelected: this.props.match.params.slug,
   };
 
   updateFilter = (filterCategory, selectedValue) => {
@@ -19,24 +20,38 @@ class Reports extends Component {
   };
 
   componentDidMount() {
-    if (!this.props.articles.length) {
+    console.log('this.props', this.props, 'this.state', this.state);
+    let { articles } = this.props;
+    const tagSelected = this.state.tagSelected;
+    articles = tagSelected
+      ? articles.filter(article =>
+          article.tags.some(tag => tag.slug === tagSelected)
+        )
+      : articles;
+
+    if (!articles.length) {
       return this.props.history.push('/reports');
     }
-    this.props.issue && this.props.issue.title
-      ? (document.title = `Local Majority | Reports | ${
-          this.props.issue.title
-        }`)
+    this.props.tag
+      ? (document.title = `Local Majority | Reports | ${slugToTitleCase(
+          this.props.tag
+        )}`)
       : (document.title = 'Local Majority | Reports');
   }
 
   render() {
-    const { issue, articles, issuesMasterList, statesMasterList } = this.props;
+    const { tagsMasterList, statesMasterList } = this.props;
+    let { articles } = this.props;
+    const tagSelected = this.state.tagSelected;
+    articles = tagSelected
+      ? articles.filter(article =>
+          article.tags.some(tag => tag.slug === tagSelected)
+        )
+      : articles;
     const featuredArticle = articles.length && articles[0] ? articles[0] : [];
     const otherArticles =
       articles.length && articles.slice(1) ? articles.slice(1) : [];
-    const subIssues = issue ? issue.subIssues : [];
-    // console.log('subIssues', subIssues);
-    // console.log('this.props', this.props);
+    const allTags = tagsMasterList ? tagsMasterList.map(tag => tag.slug) : [];
     return (
       <article className="Reports">
         <Section
@@ -65,14 +80,14 @@ class Reports extends Component {
         >
           <div className="row">
             <div className="col">
-              {issuesMasterList && (
+              {allTags && (
                 <Aux>
                   <h3 className="text-center">Topics</h3>
                   <Filters>
                     <ButtonlessFilters
-                      filterCategory="issueSelected"
-                      passedParam={this.state.issueSelected}
-                      masterList={issuesMasterList}
+                      filterCategory="tagSelected"
+                      passedParam={this.state.tagSelected}
+                      masterList={allTags}
                       updateFilter={this.updateFilter}
                     />
                   </Filters>
@@ -81,116 +96,36 @@ class Reports extends Component {
             </div>
           </div>
         </Section>
+        <Section
+          hasContainer={true}
+          spacingAbove={0}
+          spacingBelow={0}
+          background=""
+        >
+          <h2 className="text-center">{this.props.match.params.slug}</h2>
 
-        {issue ? (
-          <Aux>
-            <Section
-              hasContainer={true}
-              spacingAbove={3}
-              spacingBelow={0}
-              background=""
-            >
-              <h2 className="text-center">{issue.title}</h2>
-              <div className="row">
-                <div className="col-12">
-                  {subIssues &&
-                    subIssues.length && (
-                      <Aux>
-                        <h3>Subtopics:</h3>
-                        <Filters>
-                          <ButtonlessFilters
-                            filterCategory="issueSelected"
-                            passedParam={this.state.issueSelected}
-                            masterList={subIssues.map(
-                              subIssue => subIssue.slug
-                            )}
-                            updateFilter={this.updateFilter}
-                          />
-                        </Filters>
-                      </Aux>
-                    )}
-                </div>
-              </div>
-            </Section>
-            <Section
-              hasContainer={true}
-              spacingAbove={0}
-              spacingBelow={0}
-              background=""
-            >
-              <FeaturedReport article={featuredArticle} />
-            </Section>
+          <FeaturedReport article={featuredArticle} tagRoute="reports" />
 
-            {subIssues.map(subIssue => (
-              <Section
-                key={subIssue.title}
-                hasContainer={true}
-                spacingAbove={0}
-                spacingBelow={0}
-                background=""
-              >
-                <div className="row">
-                  <h4 className="col-12">{subIssue.title}</h4>
-                  {articles.map(article => {
-                    let articleThumbnail = article.thumbnail
-                      ? article.thumbnail
-                      : 'https://placekitten.com/400/300';
-                    let cardTags = article && article.tags ? article.tags : [];
-                    return article.tags.includes(subIssue.slug) ? (
-                      <GridXSmallIsOneSmallIsThree key={article.slug}>
-                        <ArticleCard
-                          article={article}
-                          slug={article.slug}
-                          imageSrc={articleThumbnail}
-                          title={article.title}
-                          author={article.author}
-                          articleType={article.articleType}
-                          updatedAt={article.updatedAt}
-                          tagRoute="reports"
-                          tags={cardTags}
-                        />
-                      </GridXSmallIsOneSmallIsThree>
-                    ) : null;
-                  })}
-                </div>
-              </Section>
-            ))}
-          </Aux>
-        ) : (
-          <Section
-            hasContainer={true}
-            spacingAbove={0}
-            spacingBelow={0}
-            background=""
-          >
-            <h2 className="text-center">{this.props.match.params.slug}</h2>
-
-            <FeaturedReport article={featuredArticle} />
-            <div className="row">
-              {otherArticles.map((article, i) => {
-                let articleThumbnail = article.thumbnail
-                  ? article.thumbnail
-                  : 'https://placekitten.com/400/300';
-                let cardTags = article && article.tags ? article.tags : [];
-                return (
-                  <GridXSmallIsOneSmallIsThree key={article.slug}>
-                    <ArticleCard
-                      article={article}
-                      slug={article.slug}
-                      imageSrc={articleThumbnail}
-                      title={article.title}
-                      author={article.author}
-                      articleType={article.articleType}
-                      updatedAt={article.updatedAt}
-                      tagRoute="reports"
-                      tags={cardTags}
-                    />
-                  </GridXSmallIsOneSmallIsThree>
-                );
-              })}
-            </div>
-          </Section>
-        )}
+          <div className="row">
+            {otherArticles.map((article, i) => {
+              let cardTags = article && article.tags ? article.tags : [];
+              return (
+                <GridXSmallIsOneSmallIsThree key={article.slug}>
+                  <ArticleCard
+                    article={article}
+                    slug={article.slug}
+                    imageSrc={article.imageSm}
+                    title={article.title}
+                    author={article.author}
+                    updatedAt={article.updatedAt}
+                    tagRoute="reports"
+                    tags={cardTags}
+                  />
+                </GridXSmallIsOneSmallIsThree>
+              );
+            })}
+          </div>
+        </Section>
       </article>
     );
   }
