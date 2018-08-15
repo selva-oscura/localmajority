9707e92c41838c0193872614a33125faffdc3ec9
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-08-14 23:10:05 -0700

    State: updated to reflect new API

--- src/components/States/State.js
+++ src/components/States/State.js
@@ -7,7 +7,6 @@ import BasicCard from '../common/Cards/BasicCard';
 import GridXSmallIsOneSmallIsThree from '../common/Grids/GridXSmallIsOneSmallIsThree';
 import Loading from '../common/Loading';
 import Offline from '../common/Offline';
-import Primer from '../common/Primers/Primer';
 import Section from '../common/Section/Section';
 import { getMostRecentUpdateTimestamp } from '../../utils/functions';
 import loremIpsum from '../../data/loremIpsum';
@@ -16,6 +15,7 @@ class State extends Component {
   // redirect to /states if /states/:slug is not a state for which we have information
   // (shouldn't be called if picking state from selector on page, but if directly typing in url or following faulty link this will redirect to the default page)\
   componentDidMount() {
+    // console.log('this.props for STATE!', this.props);
     const state = this.props.stateDetail
       ? this.props.stateDetail
       : this.props.state;
@@ -32,18 +32,25 @@ class State extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    // console.log('this.props for STATE from componentDidUpdate!', this.props);
     // only consider updating localStorage if query is resolved and successful
-    if (this.props.StateDetailBySlug.State) {
+    if (this.props.StateDetailBySlug.states) {
       const mostRecentUpdateToStateDetailBySlug = getMostRecentUpdateTimestamp(
-        this.props.StateDetailBySlug.State
+        this.props.StateDetailBySlug.states[0]
       );
       // only update localStorage if no seatDetail (freeze-dried record passed to component by App) or if the timestamp for SeatDetailBySlug (grapql query) includes data newer than timestamp in seatDetail(freeze-dried record)
       if (
         !this.props.stateDetail ||
         this.props.stateDetail.timestamp < mostRecentUpdateToStateDetailBySlug
       ) {
-        let now = new Date().getTime();
-        let details = { ...this.props.StateDetailBySlug.State };
+        const now = new Date().getTime();
+        const details = this.props.StateDetailBySlug.states.map(state => {
+          const imageHero =
+            state.imageHero && state.imageHero.url ? state.imageHero.url : null;
+          const imageMap =
+            state.imageMap && state.imageMap.url ? state.imageMap.url : null;
+          return { ...state, imageHero, imageMap };
+        })[0];
         details.timestamp = now;
         this.props.updateStateDetail(
           'statesDetails',
@@ -111,10 +118,26 @@ class State extends Component {
         >
           <div className="row">
             <h2 className="text-center col-12">{state.title}</h2>
-            {state.content ? (
-              <Primer primer={state} />
+            {state.summary_html ? (
+              <div dangerouslySetInnerHTML={{ __html: state.summary_html }} />
             ) : (
-              loremIpsum.map((p, i) => formatLoremIpsum(p, i))
+              [
+                <h2 className="col-12">Summary</h2>,
+                <p className="col-12">A Summary should go here</p>,
+              ]
+            )}
+            {state.content_html ? (
+              <div dangerouslySetInnerHTML={{ __html: state.content_html }} />
+            ) : (
+              [
+                <h2 className="col-12">Content</h2>,
+                <p className="col-12 tertiary-text-color">
+                  Content Really should be here, but it looks like the database
+                  needs to be flushed out for {state.title}, so here is some
+                  filler
+                </p>,
+                loremIpsum.map((p, i) => formatLoremIpsum(p, i)),
+              ]
             )}
           </div>
         </Section>
@@ -134,28 +157,28 @@ class State extends Component {
               See <Link to="/reports">here</Link> for our latest in-depth
               research reports supporting progressive state district campaigns.
             </p>
-            {articles &&
-              articles.length &&
-              articles.slice(0, 3).map(article => {
-                let cardTags = article && article.tags ? article.tags : [];
-                let articleThumbnail = article.thumbnail
-                  ? article.thumbnail
-                  : 'https://placekitten.com/200/150';
+            {articles && articles.length
+              ? articles.slice(0, 3).map(article => {
+                  let cardTags = article && article.tags ? article.tags : [];
+                  let articleThumbnail = article.thumbnail
+                    ? article.thumbnail
+                    : 'https://placekitten.com/200/150';
 
-                return (
-                  <GridXSmallIsOneSmallIsThree key={article.slug}>
-                    <ArticleCard
-                      slug={article.slug}
-                      imageSrc={articleThumbnail}
-                      title={article.title}
-                      author={article.author}
-                      updatedAt={article.updatedAt}
-                      tagRoute="reports"
-                      tags={cardTags}
-                    />
-                  </GridXSmallIsOneSmallIsThree>
-                );
-              })}
+                  return (
+                    <GridXSmallIsOneSmallIsThree key={article.slug}>
+                      <ArticleCard
+                        slug={article.slug}
+                        imageSrc={articleThumbnail}
+                        title={article.title}
+                        author={article.author}
+                        updatedAt={article.updatedAt}
+                        tagRoute="reports"
+                        tags={cardTags}
+                      />
+                    </GridXSmallIsOneSmallIsThree>
+                  );
+                })
+              : null}
           </div>
         </Section>
         <Section
@@ -175,31 +198,17 @@ class State extends Component {
               and help flip seats blue in State Houses and Senates.
             </p>
             {candidates &&
-              candidates.map((candidate, i) => {
-                const headshotUrl =
-                  candidate.headshotId && candidate.headshotId.url
-                    ? candidate.headshotId.url
-                    : null;
-                const seatTitle =
-                  candidate &&
-                  candidate.contestId &&
-                  candidate.contestId.seatId &&
-                  candidate.contestId.seatId.title
-                    ? candidate.contestId.seatId.title
-                    : null;
-
-                return (
-                  <GridXSmallIsOneSmallIsThree key={i}>
-                    <BasicCard
-                      title={candidate.title}
-                      subtitle={seatTitle}
-                      route={`candidates/${candidate.state.title}`}
-                      slug={candidate.slug}
-                      imageSrc={headshotUrl}
-                    />
-                  </GridXSmallIsOneSmallIsThree>
-                );
-              })}
+              candidates.map((candidate, i) => (
+                <GridXSmallIsOneSmallIsThree key={i}>
+                  <BasicCard
+                    title={candidate.title}
+                    subtitle={candidate.seatShortTitle}
+                    route={`candidates/${candidate.state.slug}`}
+                    slug={candidate.slug}
+                    imageSrc={candidate.imageSm}
+                  />
+                </GridXSmallIsOneSmallIsThree>
+              ))}
           </div>
         </Section>
       </article>
@@ -211,7 +220,8 @@ export default compose(
   graphql(graphQLAPI.queries.StateDetailBySlug, {
     name: 'StateDetailBySlug',
     options: props => {
-      let slug = props.match.params.slug.toLowerCase();
+      // console.log('props.match', props.match);
+      let slug = props.match.params.slug;
       return { variables: { slug } };
     },
   })
